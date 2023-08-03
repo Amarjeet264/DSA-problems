4ff2da723f139dd7c669ac79c7ec6a62da1f07b8
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2023-08-03 15:39:08 +0530

    Time: 27 ms (83.66%) | Memory: 6.4 MB (26.80%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -40,6 +40,7 @@ class Solution {
         return true;
     }
     bool calls(vector<vector<char>>& board,int c){
+        // if(c>=9)return true;
         for(int r=0;r<9;r++){
             for(int c=0;c<9;c++){
                 if(board[r][c]=='.'){
