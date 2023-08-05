946f76525628f82a9476624e55db6bae92a180fa
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2023-08-05 15:02:38 +0530

    Time: 52 ms (91.86%) | Memory: 442 MB (5.00%) - LeetSync

---
+++ 403-frog-jump/frog-jump.cpp
@@ -0,0 +1,34 @@
+class Solution {
+    bool rec(unordered_map<int,int>&mp,int idx,int jump,vector<int>&stones,vector<vector<long long>>&dp){
+        int pos=stones[idx]+jump;
+        if(pos==stones.back()){
+            return true;
+        }
+        if(mp.find(pos)==mp.end()){
+            return false;
+        }
+        if(dp[idx][jump]!=-1){
+            return dp[idx][jump];
+        }
+        int naya=mp[pos];
+        bool ans=false;
+        if(jump>1){
+            ans=ans||rec(mp,naya,jump-1,stones,dp);
+        }
+        ans=ans||rec(mp,naya,jump,stones,dp);
+        ans=ans||rec(mp,naya,jump+1,stones,dp);
+        return dp[idx][jump]= ans;
+    }
+public:
+    bool canCross(vector<int>& stones) {
+        int n=stones.size();
+        
+        unordered_map<int,int>mp;
+        
+        for(int i=0;i<n;i++){
+            mp[stones[i]]=i; 
+        }
+        vector<vector<long long>>dp(n,vector<long long>(n,-1));
+        return rec(mp,0,1,stones,dp);
+    }
+};
