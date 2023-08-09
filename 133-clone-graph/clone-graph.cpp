216e6970ce0ae492c74ec665e6140af842ad63c2
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2023-08-10 02:46:41 +0530

    Time: 7 ms (37.22%) | Memory: 9 MB (40.63%) - LeetSync

---
+++ 133-clone-graph/clone-graph.cpp
@@ -0,0 +1,54 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+    void dfs(Node* curr,Node* node,vector<Node*>&mp){
+        mp[node->val]=node;
+        for(auto ele:curr->neighbors){
+            if(mp[ele->val]==NULL){
+                Node* naya=new Node(ele->val);
+                (node->neighbors).push_back(naya);
+                dfs(ele,naya,mp);
+            }
+            else{
+                (node->neighbors).push_back(mp[ele->val]);
+            }
+        }
+    }
+public:
+    Node* cloneGraph(Node* node) {
+        if(node==NULL)return node;
+        vector<Node*>mp(1000,NULL);
+        Node* copy=new Node(node->val);
+        mp[node->val]=copy;
+        for(auto curr:node->neighbors){
+            if(mp[curr->val]==NULL){
+                Node* naya=new Node(curr->val);
+                (copy->neighbors).push_back(naya);
+                dfs(curr,naya,mp);
+            }
+            else{
+                (copy->neighbors).push_back(mp[curr->val]);
+            }
+        }
+        return copy;
+    }
+};
