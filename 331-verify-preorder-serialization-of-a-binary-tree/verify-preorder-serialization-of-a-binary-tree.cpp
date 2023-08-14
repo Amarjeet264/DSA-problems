dc67f2ff907da64cda42e83956344abc12365284
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2023-08-15 00:22:59 +0530

    Time: 3 ms (72.85%) | Memory: 6.8 MB (79.87%) - LeetSync

--- 331-verify-preorder-serialization-of-a-binary-tree/verify-preorder-serialization-of-a-binary-tree.cpp
+++ 331-verify-preorder-serialization-of-a-binary-tree/verify-preorder-serialization-of-a-binary-tree.cpp
@@ -5,12 +5,10 @@ public:
         int node=1;
         string str;
         stringstream ss(preorder);
-        // getline(ss,str,',');
         while(getline(ss,str,',')){
             node--;
             if(node<0)return false;
-            if(str!="#")node+=2;
-            // getline(ss,str,',');
+            if(str!="#")node+=2; 
         }
         return node==0;
     }
