bd1cad084135f19046e44a371925b7e206558ea5
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2023-08-16 23:34:21 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (53.69%) - LeetSync

---
+++ 140-word-break-ii/word-break-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    vector<string>ans;
+    set<string>st;
+    void find(int idx,string temp,string s){
+        // cout<<temp<<" ";
+        if(idx>=s.length()){
+            temp.pop_back();
+            ans.push_back(temp);
+            return;
+        }
+
+        for(int i=idx;i<s.length();i++){
+            if(st.count(s.substr(idx,i-idx+1))){
+                find(i+1,temp+s.substr(idx,i-idx+1)+" ",s);
+            }
+        }
+    }
+public:
+    vector<string> wordBreak(string s, vector<string>& word) {
+        for(int i=0;i<word.size();i++){
+            st.insert(word[i]);
+        }
+        find(0,"",s);
+        return ans;
+    }
+};
