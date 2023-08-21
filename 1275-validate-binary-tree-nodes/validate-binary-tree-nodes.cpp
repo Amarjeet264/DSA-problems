30f86563dea4b30c25a543e0502f3f39c5b88b6e
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2023-08-21 15:53:09 +0530

    Time: 65 ms (44.60%) | Memory: 41.2 MB (33.56%) - LeetSync

---
+++ 1275-validate-binary-tree-nodes/validate-binary-tree-nodes.cpp
@@ -0,0 +1,81 @@
+class Disjointset{
+    public:
+    vector<int>parent,size;
+    Disjointset(int n){
+        parent.resize(n+1,0);
+        size.resize(n+1);
+
+        for(int i=0;i<=n;i++){
+            parent[i]=i;
+            size[i]=1;
+        }
+    }
+    int findulpar(int node){
+        if(parent[node]==node){
+            return node;
+        }
+        return parent[node]=findulpar(parent[node]);
+    }
+    void unionbysize(int u,int v){
+        int ulpu=findulpar(u);
+        int ulpv=findulpar(v);
+        if(ulpu==ulpv){
+            return;
+        }
+        if(size[ulpu]<size[ulpv]){
+            parent[ulpu]=ulpv;
+            size[ulpv]+=size[ulpu];
+        }
+        else{
+            parent[ulpv]=ulpu;
+            size[ulpu]+=size[ulpv];
+        }
+    }
+};
+class Solution {
+public:
+    bool validateBinaryTreeNodes(int n, vector<int>& left, vector<int>& right) {
+        vector<int>adj[n];
+        vector<int>in(n,0);
+        for(int i=0;i<n;i++){
+            if(left[i]!=-1){
+                adj[i].push_back(left[i]);
+                in[left[i]]++;
+            }
+            if(right[i]!=-1){
+                adj[i].push_back(right[i]);
+                in[right[i]]++;
+            }
+        }
+        queue<int>q;
+        int root=-1;
+        for(int i=0;i<n;i++){
+            if(in[i]==0){
+                root=i;
+                break;
+            }
+        }
+        if(root==-1)return false;
+        q.push(root);
+        vector<int>vis(n,0);
+        vis[root]=1;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            int cnt=0;
+            for(auto it:adj[node]){
+                if(vis[it]){
+                    return false;
+                }
+                vis[it]=1;
+                q.push(it);
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
