e752505371dd969e93be31e4f0d8d9c613908cf4
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2023-08-22 02:01:18 +0530

    Time: 93 ms (54.33%) | Memory: 50 MB (52.39%) - LeetSync

---
+++ 1309-sort-items-by-groups-respecting-dependencies/sort-items-by-groups-respecting-dependencies.cpp
@@ -0,0 +1,81 @@
+class Solution {
+public:
+    vector<int> sortItems(int n, int m, vector<int>& group, vector<vector<int>>& before) {
+        vector<int>adj[n];
+        vector<int>in(n,0);
+        for(int i=0;i<before.size();i++){
+            for(auto it:before[i]){
+                adj[it].push_back(i);
+                in[i]++;
+            }
+        }
+        queue<int>q;
+        for(int i=0;i<n;i++){
+            if(in[i]==0){
+                q.push(i);
+            }
+        }
+        vector<int>a;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            a.push_back(node);
+            for(auto it:adj[node]){
+                in[it]--;
+                if(in[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+        if(a.size()!=n){
+            return {};
+        }
+        unordered_map<int,int>grp;
+        for(int i=0;i<group.size();i++){
+            if(group[i]==-1){
+                grp[i]=m++;
+            }
+            else{
+                grp[i]=group[i];
+            }
+        }
+        vector<int>adj1[m];
+        vector<int>in1(m,0);
+        for(int i=0;i<before.size();i++){
+            for(int j=0;j<before[i].size();j++){
+                if(grp[i]!=grp[before[i][j]]){
+                    adj1[grp[before[i][j]]].push_back(grp[i]);
+                    in1[grp[i]]++;
+                }
+            }
+        }
+        unordered_map<int,vector<int>>mp1;
+        for(int i=0;i<n;i++){
+            mp1[grp[a[i]]].push_back(a[i]);
+        }
+        queue<int>q1;
+        for(int i=0;i<m;i++){
+            if(in1[i]==0){
+                // cout<<i<<" ";
+                q1.push(i);
+            }
+        }
+        // cout<<"dnd"<<" ";
+        vector<int>ans;
+        while(!q1.empty()){
+            int gr=q1.front();
+            q1.pop();
+            for(auto it:mp1[gr]){
+                ans.push_back(it);
+            }
+            for(auto it:adj1[gr]){
+                in1[it]--;
+                if(in1[it]==0){
+                    q1.push(it);
+                }
+            }
+        }
+        if(ans.size()!=n)return {};
+        return ans;
+    }
+};
