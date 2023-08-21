69735cba63198fee7fef88c1c580be70fd892ab6
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2023-08-22 04:09:34 +0530

    Time: 537 ms (25.89%) | Memory: 166.9 MB (11.78%) - LeetSync

---
+++ 2979-maximize-the-profit-as-the-salesman/maximize-the-profit-as-the-salesman.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maximizeTheProfit(int n, vector<vector<int>>& offer) {
+        vector<int>dp(n,0);
+        map<int,vector<pair<int,int>>>mp;
+        for(int i=0;i<offer.size();i++){
+            mp[offer[i][0]].push_back({offer[i][1],offer[i][2]});
+        }
+        for(int i=0;i<n;i++){
+            if(i){
+                dp[i]=max(dp[i],dp[i-1]) ;
+            }
+
+            for(auto &x:mp[i]){
+                if(i==0){
+                    dp[x.first]=max(dp[x.first],x.second) ;
+                }
+                else{
+                    dp[x.first]=max(dp[x.first],dp[i-1]+x.second) ;
+                }
+            }
+        }
+        return dp[n-1];
+    }
+};
