4380051404c38aa8e73fe90947011dd25e480390
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2023-08-21 20:54:51 +0530

    Time: 14 ms (87.20%) | Memory: 13.4 MB (57.60%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.cpp
+++ 459-repeated-substring-pattern/repeated-substring-pattern.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     bool repeatedSubstringPattern(string s) {
        string temp = s+s;
-       return temp.substr(1, temp.length()-2).find(s)!=-1;
+       string cont=temp.substr(1, temp.length()-2);
+       return cont.find(s)!=-1;
     }
 };
