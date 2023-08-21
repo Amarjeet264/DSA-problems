c8c3770d0dd45e273b66aa06ca14489c6e4e17df
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2023-08-21 20:51:47 +0530

    Time: 17 ms (70.36%) | Memory: 13.4 MB (57.60%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.cpp
+++ 459-repeated-substring-pattern/repeated-substring-pattern.cpp
@@ -1,27 +1,7 @@
 class Solution {
 public:
     bool repeatedSubstringPattern(string s) {
-        int n=s.length();
-       for(int i=1;i<=s.length()/2;i++){
-           if(n%i==0){
-                string str="";
-                for(int j=0;j<s.length()/i;j++){
-                    str+=s[j];
-                    string dup=str;
-                    
-                    if(str==s||str.length()>(s.length()/2)+1){
-                        break;
-                    }
-                    while(str.length()<s.length()){
-                        str+=dup;
-                    }
-                    if(str==s){
-                        return true;
-                    }
-                    str=dup;
-                }
-           }
-       }
-       return false;
+       string temp = s+s;
+       return temp.substr(1, temp.length()-2).find(s)!=-1;
     }
 };
