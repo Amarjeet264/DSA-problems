a50659762fb53a4a1255de87beeda0eee77e18f8
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2023-08-22 09:48:30 +0530

    Time: 444 ms (67.18%) | Memory: 163.8 MB (12.52%) - LeetSync

--- 2979-maximize-the-profit-as-the-salesman/maximize-the-profit-as-the-salesman.cpp
+++ 2979-maximize-the-profit-as-the-salesman/maximize-the-profit-as-the-salesman.cpp
@@ -6,11 +6,8 @@ public:
         for(int i=0;i<offer.size();i++){
             mp[offer[i][0]].push_back({offer[i][1],offer[i][2]});
         }
-        for(int i=0;i<n;i++){
-            if(i>0){
-                dp[i]=max(dp[i],dp[i-1]) ;
-            }
 
+        for(int i=0;i<n;i++){
             for(auto &x:mp[i]){
                 if(i==0){
                     dp[x.first]=max(dp[x.first],x.second) ;
@@ -19,7 +16,11 @@ public:
                     dp[x.first]=max(dp[x.first],dp[i-1]+x.second) ;
                 }
             }
+            if(i>0){
+                dp[i]=max(dp[i],dp[i-1]) ;
+            }
         }
+
         return dp[n-1];
     }
 };
