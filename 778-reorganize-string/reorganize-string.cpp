12e1718107293809a57fd35c0763e5ccbc28a727
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2023-08-24 22:26:38 +0530

    Time: 0 ms (100.00%) | Memory: 6.4 MB (21.42%) - LeetSync

--- 778-reorganize-string/reorganize-string.cpp
+++ 778-reorganize-string/reorganize-string.cpp
@@ -10,38 +10,27 @@ public:
             pq.push({it.second,it.first});
         }
         string ans="";
-        if(pq.size()==1&&pq.top().first==1){
-            return ans+pq.top().second;
-        }
-        if(pq.size()<=1){
-            return ans;
-        }
-        while(pq.size()>1){
+        while(pq.size()>0){
             int ff=pq.top().first;
             char fir=pq.top().second;
             pq.pop();
-
+            ans+=fir;
+            if(pq.empty()){
+                continue;
+            }
             int sf=pq.top().first;
             char sec=pq.top().second;
             pq.pop();
             if(ff-1>0){
                 pq.push({ff-1,fir});
             }
-            ans+=fir;
             ans+=sec;
             if(sf-1>0){
                 pq.push({sf-1,sec});
             }
         }
-        if(!pq.empty()&&ans[ans.size()-1]==pq.top().second){
+        if(ans.size()!=s.size()){
             return "";
-        }
-        if(!pq.empty()&&pq.top().first>1){
-            return "";
-        }
-        if(!pq.empty()&&ans[ans.size()-1]!=pq.top().second){
-            // return "";
-           ans+=pq.top().second;
         }
         return ans;
     }
