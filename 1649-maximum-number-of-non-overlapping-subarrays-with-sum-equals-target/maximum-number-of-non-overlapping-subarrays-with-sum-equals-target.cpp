3d871050ccd1099eca65a1fab78b06ebef55332f
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2023-08-26 13:05:03 +0530

    Time: 188 ms (72.36%) | Memory: 88.4 MB (30.18%) - LeetSync

--- 1649-maximum-number-of-non-overlapping-subarrays-with-sum-equals-target/maximum-number-of-non-overlapping-subarrays-with-sum-equals-target.cpp
+++ 1649-maximum-number-of-non-overlapping-subarrays-with-sum-equals-target/maximum-number-of-non-overlapping-subarrays-with-sum-equals-target.cpp
@@ -5,12 +5,14 @@ public:
         int n=nums.size();
         int sum=0;
         int cnt=0;
+        unordered_set<int>mp1;
+        mp1.insert(0);
         mp.insert(0);
         for(int i=0;i<n;i++){
             sum+=nums[i];
             if(mp.find(sum-target)!=mp.end()){
                 cnt++;
-                mp={0};
+                mp=mp1;
                 sum=0;
                 continue;
             }
