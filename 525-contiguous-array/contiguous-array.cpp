a7b5f4a71a61e0c169f3067c5f20fc52b8d68c45
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2023-08-26 16:21:56 +0530

    Time: 83 ms (94.37%) | Memory: 83.7 MB (87.73%) - LeetSync

--- 525-contiguous-array/contiguous-array.cpp
+++ 525-contiguous-array/contiguous-array.cpp
@@ -5,14 +5,14 @@ public:
         int maxi=0;
         int sum=0;
         unordered_map<int,int>mp;
-        mp[0]=0;
+        mp[0]=-1;
         for(int i=0;i<n;i++){
             sum+=nums[i]==1?1:-1;
             if(mp.find(sum)!=mp.end()){
-                maxi=max(maxi,i-mp[sum]+1);
+                maxi=max(maxi,i-mp[sum]);
                 continue;
             }
-            mp[sum]=i+1;
+            mp[sum]=i;
         }
         return maxi;
     }
