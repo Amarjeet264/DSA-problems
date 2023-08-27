fcf6b265f0a569c05d184d82cda7253b02061984
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2023-08-28 01:55:20 +0530

    Time: 12 ms (59.60%) | Memory: 5.9 MB (45.89%) - LeetSync

---
+++ 668-kth-smallest-number-in-multiplication-table/kth-smallest-number-in-multiplication-table.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    int count(int x,int m,int n,int k){
+        int count=0;
+        for(int i=1;i<=m;i++){
+          count+=min(x/i,n);
+        }
+        return count;
+    }
+public:
+    int findKthNumber(int m, int n, int k) {
+        int low=0;
+        int high=m*n;
+        while(low<high){
+            int mid=(low+high)/2;
+            int x=count(mid,m,n,k);
+            if(x<k){
+                low=mid+1;
+            }
+            else{
+                high=mid;
+            }
+        }
+        return low;
+    }
+};
