077d3773043dd3271f50defc62c5ae060f13d7a3
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2023-08-28 11:05:15 +0530

    Time: 3 ms (36.88%) | Memory: 6.8 MB (67.22%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -1,51 +1,32 @@
 class MyStack {
+    queue<int>q;
 public:
-queue<int> q1;
-queue<int> q;
     MyStack() {
         
     }
     
     void push(int x) {
         q.push(x);
+        for(int i=0;i<q.size()-1;i++){
+            q.push(q.front());
+            q.pop();
+        }
     }
     
     int pop() {
-        while(q.size()>1){
-            q1.push(q.front());
-            q.pop();
-        }
         int x=q.front();
         q.pop();
-        while(!q1.empty()){
-            q.push(q1.front());
-            q1.pop();
-        }
         return x;
     }
     
     int top() {
-        while(q.size()>1){
-            q1.push(q.front());
-            q.pop();
-        }
         int x=q.front();
-        q1.push(x);
-        q.pop();
-        while(!q1.empty()){
-            q.push(q1.front());
-            q1.pop();
-        }
         return x;
     }
     
     bool empty() {
-        if(q.size()==0){
-            return 1;
-        }
-        else{
-            return 0;
-        }
+        if(q.size()==0)return true;
+        return false;
     }
 };
 
