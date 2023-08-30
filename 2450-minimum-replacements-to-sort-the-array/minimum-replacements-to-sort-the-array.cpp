e56a0e7a774a3cc5e80e470cdb079358423221f1
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2023-08-30 12:08:40 +0530

    Time: 93 ms (70.57%) | Memory: 54.6 MB (87.69%) - LeetSync

--- 2450-minimum-replacements-to-sort-the-array/minimum-replacements-to-sort-the-array.cpp
+++ 2450-minimum-replacements-to-sort-the-array/minimum-replacements-to-sort-the-array.cpp
@@ -7,9 +7,7 @@ public:
         for(int i=n-2;i>=0;i--){
             if(nums[i]>prev){
                 long long int tot=(nums[i]-1)/prev;
-                cout<<tot<<" ";
                 prev=nums[i]/(tot+1);
-                cout<<prev<<" ";
                 ans+=tot;
             }
             else{
