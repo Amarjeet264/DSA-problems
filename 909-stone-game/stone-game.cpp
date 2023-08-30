d7bd3cd45e43c97d191380d29d0b4ade86b2c35f
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2023-08-31 01:51:05 +0530

    Time: 5 ms (60.34%) | Memory: 16.6 MB (15.65%) - LeetSync

---
+++ 909-stone-game/stone-game.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    vector<vector<int>>dp;
+    bool stone(int i,int j,vector<int>&piles,int krishna,int radha,bool cankrishna){
+        if(i>=j){
+            return krishna>radha;
+        }
+        if(dp[i][j]!=-1)return dp[i][j];
+        if(cankrishna){
+            return dp[i][j]= stone(i+1,j,piles,krishna+piles[i],radha,0)||stone(i,j-1,piles,krishna+piles[j],radha,0);
+        }
+        else
+          return dp[i][j]= stone(i+1,j,piles,krishna,radha+piles[i],1)||stone(i,j-1,piles,krishna,radha+piles[j],1);
+    }
+public:
+    bool stoneGame(vector<int>& piles) {
+        int n=piles.size();
+        dp=vector<vector<int>>(n,vector<int>(n,-1));
+        return stone(0,n-1,piles,0,0,1);
+    }
+};
