c0f0eaf9d1b8a30764073618e9017628e02943ba
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2023-09-01 03:35:29 +0530

    Time: 19 ms (69.72%) | Memory: 11.3 MB (51.28%) - LeetSync

--- 1240-stone-game-ii/stone-game-ii.cpp
+++ 1240-stone-game-ii/stone-game-ii.cpp
@@ -1,71 +1,27 @@
 class Solution {
-//     vector<vector<vector<int>>>dp;
-//     int stone(vector<int>&pre,vector<int>&piles,int i,int currtakemax,bool can){
-//         if(i>=piles.size()){
-//             return 0;
-//         }
-//         int maxi=0;
-//         int alice=INT_MAX;
-//         for(int k=0;k<2*currtakemax;k++){
-//             if(can){
-//                 if(i+k<piles.size()){
-//                     maxi=max(maxi,pre[i+k]+stone(pre,piles,i+k+1,max(currtakemax,k+1),0));
-//                 }
-//             }
-//             else{
-//                 alice=min(alice,stone(pre,piles,i+k+1,max(currtakemax,k+1),1));
-//             }
-//         }
-//         return can?maxi:alice;
-//     }
-// public:
-//     int stoneGameII(vector<int>& piles) {
-//         int sum=0;
-//         int n=piles.size();
-//         vector<int>pre;
-//         dp=vector<vector<vector<int>>>(n,vector<vector<int>>(n,vector<int>(2,-1)));
-//         for(int i=0;i<n;i++){
-//             sum+=piles[i];
-//             pre.push_back(sum);
-//         }
-//         return stone(pre,piles,0,1,1);
-//     }
-vector<vector<vector<int>>>dp;
-    int getPrefix(int start,int size,vector<int>&prefix){
-        int end=prefix.size()-1;
-        if(start+size<prefix.size())
-            end=start+size-1;
-        return start? prefix[end]-prefix[start-1]: prefix[end];
-     }
-    int stone(vector<int>&pre,vector<int>&piles,int i,int currtakemax,bool can){
+    vector<vector<int>>dp;
+    int stone(vector<int>&piles,int i,int currtakemax){
         if(i>=piles.size()){
             return 0;
         }
-        int maxi=0;
-        int alice=INT_MAX;
-        if(dp[i][currtakemax][can]!=-1)return dp[i][currtakemax][can];
+        int maxi=-1e9;
+        int tot=0;
+        if(dp[i][currtakemax]!=-1)return dp[i][currtakemax];
         for(int k=0;k<2*currtakemax;k++){
-            if(can){
-                if(i+k<piles.size()){
-                    maxi=max(maxi,getPrefix(i,k+1,pre)+stone(pre,piles,i+k+1,max(currtakemax,k+1),0));
-                }
-            }
-            else{
-                alice=min(alice,stone(pre,piles,i+k+1,max(currtakemax,k+1),1));
-            }
+            if(i+k<piles.size())tot+=piles[i+k];
+            maxi=max(maxi,tot-stone(piles,i+k+1,max(currtakemax,1+k)));
         }
-        return dp[i][currtakemax][can]= can?maxi:alice;
+        return dp[i][currtakemax]=maxi;
     }
 public:
     int stoneGameII(vector<int>& piles) {
         int sum=0;
         int n=piles.size();
-        vector<int>pre;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<piles.size();i++){
             sum+=piles[i];
-            pre.push_back(sum);
         }
-        dp=vector<vector<vector<int>>>(n,vector<vector<int>>(2*n,vector<int>(2,-1)));
-        return stone(pre,piles,0,1,1);
+        dp=vector<vector<int>>(n,vector<int>(2*n,-1));
+        int diff=stone(piles,0,1);
+        return (sum+diff)/2;
     }
 };
