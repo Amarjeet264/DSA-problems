ce7a84c554f3a979077d185dd80ba82dbf490c67
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2023-08-31 21:06:50 +0530

    Time: 48 ms (35.61%) | Memory: 14.6 MB (72.16%) - LeetSync

---
+++ 1451-minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minTaps(int n, vector<int>& ranges) {
+        vector<int>dp(n+2,1e8);
+        dp[0]=0;
+        for(int i=0;i<=n;i++){
+            for(int j=max(i-ranges[i],0);j<=min(i+ranges[i],n);j++){
+                dp[j]=min(dp[j],1+dp[max(0,i-ranges[i])]);
+            }
+        }
+        return dp[n]>=1e8? -1:dp[n];
+    }
+};
