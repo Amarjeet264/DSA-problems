625b81758d2a2076d50b46111e94209b1d1880e6
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2023-09-01 17:22:38 +0530

    Time: 531 ms (5.55%) | Memory: 33.7 MB (55.56%) - LeetSync

---
+++ 1550-find-the-kth-smallest-sum-of-a-matrix-with-sorted-rows/find-the-kth-smallest-sum-of-a-matrix-with-sorted-rows.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& mat, int k) {
+        vector<int>row=mat[0];
+        for(int i = 1; i < mat.size(); ++i) {
+            vector<int> nr;
+            for(int j = 0; j < mat[i].size(); ++j) {
+                for(auto n : row) {
+                    nr.push_back(mat[i][j] + n);
+                    cout<<mat[i][j]+n<<" ";
+                }
+            }
+            cout<<endl;
+            sort(nr.begin(), nr.end());
+            row.resize(min(k, int(nr.size())));
+            for(int x = 0; x <min(k, int(nr.size())) ;++x) {
+                row[x] = nr[x];
+                cout<<row[x]<<" ";
+            }
+            cout<<endl;
+        }
+        return row[k-1];
+    }
+};
