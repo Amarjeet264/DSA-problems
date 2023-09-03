1309bdb9f70d9039c078c7a6218417bc86499439
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2023-09-03 12:54:32 +0530

    Time: 1015 ms (48.29%) | Memory: 23.2 MB (53.44%) - LeetSync

---
+++ 1685-stone-game-v/stone-game-v.cpp
@@ -0,0 +1,33 @@
+class Solution {
+    int stone(int i,int j,vector<int>&st,vector<vector<int>>&dp){
+        if(i==j)return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+        int maxi=0;
+        for(int k=i;k<j;k++){
+            int left=0;
+            left=st[k];
+            if(i!=0)left-=st[i-1];
+            int right=0;
+            right=st[j]-st[k];
+            if(left>right){
+                maxi=max(maxi,right+stone(k+1,j,st,dp));
+            }
+            else if(left<right){
+                maxi=max(maxi,left+stone(i,k,st,dp));
+            }
+            else{
+                maxi=max(maxi,right+stone(k+1,j,st,dp));
+                maxi=max(maxi,left+stone(i,k,st,dp));
+            }
+        }
+        return dp[i][j]= maxi;
+    }
+public:
+    int stoneGameV(vector<int>& st) {
+        for(int i=1;i<st.size();i++){
+            st[i]+=st[i-1];
+        }
+        vector<vector<int>>dp(st.size(),vector<int>(st.size(),-1));
+        return stone(0,st.size()-1,st,dp);
+    }
+};
