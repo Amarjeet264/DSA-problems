c6cb386ca8f127080ca65cf69280631c0eaed6b5
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2023-09-08 20:52:46 +0530

    Time: 10 ms (70.75%) | Memory: 7.1 MB (96.07%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,24 +1,16 @@
 class Solution {
-    int coinss(int target,int i,vector<int>&coins,vector<vector<int>>&dp){
-        if(target==0){
-            return 1;
-        }
-        if(i<0){
-            return 0;
-        }
-        if(dp[i][target]!=-1){
-            return dp[i][target];
-        }
-        int nottake=coinss(target,i-1,coins,dp);
-        int take=0;
-        if(target>=coins[i]){
-            take=coinss(target-coins[i],i,coins,dp);
-        }
-        return dp[i][target]= take+nottake;
-    }
 public:
     int change(int amount, vector<int>& coins) {
-        vector<vector<int>>dp(coins.size()+1,vector<int>(amount+1,-1));
-        return coinss(amount,coins.size()-1,coins,dp);
+        vector<int>dp(amount+1,0);
+        int n=coins.size();
+        dp[0]=1;
+        for(int i=0;i<n;i++){
+            for(int j=1;j<=amount;j++){
+                if(j>=coins[i]){
+                    dp[j]+=dp[j-coins[i]];
+                }
+            }
+        }
+        return dp[amount];
     }
 };
