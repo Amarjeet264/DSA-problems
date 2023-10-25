6394ea88a1e976db17144e1c13370ddb19d1e52f
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2023-10-26 03:04:56 +0530

    Time: 8 ms (88.09%) | Memory: 16.9 MB (59.50%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
-    int jump(vector<int>& nums) {
-        int steps=0;
-        int i=0;
-        while(i<nums.size()){
-            int starti=i;
-            int endi=i+nums[i];
-            if(i>=nums.size()-1)return steps;
-            int maxi=-1;
+    int jump(vector<int>& nums) { 
+        if(nums.size()<=1)return 0;
+        int j=0;
+        int steps=1;
+        while(j<nums.size()){
+            int start=j;
+            int maxi=j+nums[j];
+            if(maxi>=nums.size()-1){
+                return steps;
+            }
             steps++;
-            if(endi>=nums.size()-1)return steps;
-            int idx=i;
-            for(int j=starti;j<=endi;j++){
-                if(maxi<j+nums[j]){
-                    maxi=j+nums[j];
-                    idx=j;
+            int idx=j;
+            for(int i=start+1;i<=start+nums[j];i++){
+                if(maxi<i+nums[i]){
+                    maxi=max(maxi,i+nums[i]);
+                    idx=i;
                 }
             }
-            if(i==idx)break;
-            i=idx;
+            if(idx==j){
+                return -1;
+            }
+            j=idx;
         }
-        return 0;
+        return -1;
     }
 };
