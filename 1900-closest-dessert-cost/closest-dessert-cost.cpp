e10cfbf45d9cc3844d700b8db574bfe7ccad50e1
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2023-10-29 12:18:16 +0530

    Time: 12 ms (77.42%) | Memory: 60.9 MB (9.32%) - LeetSync

--- 1900-closest-dessert-cost/closest-dessert-cost.cpp
+++ 1900-closest-dessert-cost/closest-dessert-cost.cpp
@@ -1,6 +1,7 @@
 class Solution {
     int ans=INT_MIN;
     int diff=INT_MAX;
+    unordered_map<int,int>mp;
     void solve(vector<int>&top,int target,int sum,int i,vector<vector<int>>&dp){
         if(diff>abs(sum-target)){
             diff=abs(sum-target);
@@ -12,13 +13,20 @@ class Solution {
         if(sum>=target||i>=top.size()){
             return ;
         }
-        if(dp[i][sum]!=-1)return ;
-        solve(top,target,sum+top[i],i+1,dp);
-        solve(top,target,sum+2*top[i],i+1,dp);
-        solve(top,target,sum,i+1,dp);
+        if(dp[i][sum]!=-1)return;
+        for(int j=i;j<top.size();j++){
+            if(mp[j]<2){
+                mp[j]++;
+                solve(top,target,sum+top[j],j,dp);
+                solve(top,target,sum+top[j],j+1,dp);
+                mp[j]--;
+                if(mp[j]==0)mp.erase(j);
+            }
+            solve(top,target,sum,j+1,dp);
+        }
         dp[i][sum]=1;
-        
     }
+        
 public:
     int closestCost(vector<int>& base, vector<int>& top, int target) {
         for(int i=0;i<base.size();i++){
