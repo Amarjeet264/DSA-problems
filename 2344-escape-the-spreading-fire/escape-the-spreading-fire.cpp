f3aed1f82c84e27fdcea0e533a64c058499835d6
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2023-11-07 15:25:29 +0530

    Time: 432 ms (12.23%) | Memory: 133.5 MB (10.91%) - LeetSync

--- 2344-escape-the-spreading-fire/escape-the-spreading-fire.cpp
+++ 2344-escape-the-spreading-fire/escape-the-spreading-fire.cpp
@@ -1,50 +1,118 @@
 class Solution {
 public:
-    int dfs(int i,int j,int t,vector<vector<int>>& vis,vector<vector<int>>& dis){
-        if(i==vis.size()-1 && j==vis[0].size()-1) return 1;
-        if(dis[i][j]==t) return 0;
-        int dx[]={0,0,1,-1};
-        int dy[]={1,-1,0,0};
-        vis[i][j]=t;
-        for(int k=0;k<4;k++){
-            int nx=dx[k]+i,ny=dy[k]+j;
-            if(nx<0 || ny<0 || nx>=vis.size() || ny>=vis[0].size()) continue;
-            if(vis[nx][ny]>t+1 && dis[nx][ny]>=t+1 && dfs(nx,ny,t+1,vis,dis)) return 1;
-        }
-        return 0;
-    }
-    int chk(int tm,vector<vector<int>>& a){
-        queue<vector<int>> q;
-        vector<vector<int>> dis(a.size(),vector<int> (a[0].size(),INT_MAX));
-        vector<vector<int>> vis(a.size(),vector<int> (a[0].size(),INT_MAX));
-        for(int i=0;i<a.size();i++){
-            for(int j=0;j<a[0].size();j++){
-                if(a[i][j]==1) q.push({i,j,0}),dis[i][j]=0;
-                else if(a[i][j]==2) dis[i][j]=-1;
+    int dr[4]={-1,0,1,0};
+    int dc[4]={0,1,0,-1};
+    bool bfs(vector<vector<int>>&aag,vector<vector<int>>&mere){
+        for(int i=0;i<aag.size();i++){
+            for(int j=0;j<aag[0].size();j++){
+                mere[i][j]=aag[i][j]-mere[i][j];
             }
         }
-        while(q.size()){
-            int sz=q.size();
-            for(int i=0;i<sz;i++){
-                int cx=q.front()[0],cy=q.front()[1],t=q.front()[2];q.pop();
-                int dx[]={0,0,1,-1};
-                int dy[]={1,-1,0,0};
-                for(int j=0;j<4;j++){
-                    int nx=dx[j]+cx,ny=dy[j]+cy;
-                    if(nx<0 || ny<0 || nx>=a.size() || ny>=a[0].size() || dis[nx][ny]<=t+1) continue;
-                    q.push({nx,ny,t+1});
-                    dis[nx][ny]=1+t;
+        queue<pair<int,int>>q;
+        q.push({0,0});
+        vector<vector<int>>vis(aag.size(),vector<int>(aag[0].size(),0));
+        vis[0][0]=1;
+        while(!q.empty()){
+            int row=q.front().first;
+            int col=q.front().second;  
+            q.pop();
+            for(int i=0;i<4;i++){
+                int nrow=row+dr[i];
+                int ncol=col+dc[i];
+                if(nrow==aag.size()-1&&ncol==aag[0].size()-1&&mere[nrow][ncol]>=0){
+                  return 1;
+                } 
+                if(nrow>=0&&ncol>=0&&nrow<aag.size()&&ncol<aag[0].size()&&vis[nrow][ncol]==0&&mere[nrow][ncol]>0){
+                    vis[nrow][ncol]=1;
+                    q.push({nrow,ncol});
                 }
             }
         }
-        return dfs(0,0,tm,vis,dis);
+        return 0;
     }
+    bool poss(int min,vector<vector<int>>grid){
+        
+        queue<pair<int,pair<int,int>>>q;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]==1){
+                    q.push({0,{i,j}});
+                }
+
+            }
+        }
+        while(!q.empty()){
+            int dis=q.front().first;
+            int row=q.front().second.first;
+            int col=q.front().second.second;
+            q.pop();
+            if(dis==min)continue;
+            for(int i=0;i<4;i++){
+                int nrow=row+dr[i];
+                int ncol=col+dc[i];
+                if(nrow>=0&&ncol>=0&&nrow<grid.size()&&ncol<grid[0].size()&&grid[nrow][ncol]==0){
+                    grid[nrow][ncol]=1;
+                    q.push({dis+1,{nrow,ncol}});
+                }
+            }
+        }
+
+        vector<vector<int>>aag(grid.size(),vector<int>(grid[0].size(),INT_MAX));
+        vector<vector<int>>mere(grid.size(),vector<int>(grid[0].size(),INT_MAX));
+        q.push({0,{0,0}});
+        mere[0][0]=0;
+
+        while(!q.empty()){
+            int dis=q.front().first;
+            int row=q.front().second.first;
+            int col=q.front().second.second;
+            q.pop();
+            for(int i=0;i<4;i++){
+                int nrow=row+dr[i];
+                int ncol=col+dc[i];
+                if(nrow>=0&&ncol>=0&&nrow<grid.size()&&ncol<grid[0].size()&&grid[nrow][ncol]==0&&mere[nrow][ncol]>dis+1){
+                    mere[nrow][ncol]=dis+1;
+                    q.push({dis+1,{nrow,ncol}});
+                }
+            }
+        }
+
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]==1){
+                    aag[i][j]=0;
+                    q.push({0,{i,j}});
+                }
+                else if(grid[i][j]==2){
+                    aag[i][j]=-1;
+                }
+            }
+        }
+
+        while(!q.empty()){
+            int dis=q.front().first;
+            int row=q.front().second.first;
+            int col=q.front().second.second;
+            q.pop();
+            for(int i=0;i<4;i++){
+                int nrow=row+dr[i];
+                int ncol=col+dc[i];
+                if(nrow>=0&&ncol>=0&&nrow<grid.size()&&ncol<grid[0].size()&&grid[nrow][ncol]==0&&aag[nrow][ncol]>dis+1){
+                    aag[nrow][ncol]=dis+1;
+                    q.push({dis+1,{nrow,ncol}});
+                } 
+            }
+        }
+        return bfs(aag,mere);
+    }
+
     int maximumMinutes(vector<vector<int>>& a) {
+        // if(!poss(0,a))return -1;
         int n=a.size(),m=a[0].size();
         int l=-1,r=n*m+100,ans=-1;
         while(r-l>1){
             int m=l+(r-l)/2;
-            if(chk(m,a)) ans=m,l=m;
+            if(poss(m,a)) ans=m,l=m;
             else r=m;
         }
         if(ans>=n*m) return 1000000000;
