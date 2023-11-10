d5c537141c9fbf4dc2a9171d23ef5a6039e54336
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2023-11-10 15:43:46 +0530

    Time: 530 ms (28.18%) | Memory: 134.5 MB (46.06%) - LeetSync

---
+++ 1866-restore-the-array-from-adjacent-pairs/restore-the-array-from-adjacent-pairs.cpp
@@ -0,0 +1,40 @@
+class Solution {
+    vector<int>ans;
+public:
+    vector<int> restoreArray(vector<vector<int>>& adj) {
+        unordered_map<int,vector<int>>mp;
+        unordered_map<int,int>in;
+        for(int i=0;i<adj.size();i++){
+            mp[adj[i][0]].push_back(adj[i][1]);
+            mp[adj[i][1]].push_back(adj[i][0]);
+            in[adj[i][1]]++;
+            in[adj[i][0]]++;
+        }
+        int node=-1;
+        for(auto it:in){
+            if(it.second==1){
+                node=it.first;
+                cout<<it.first<<" ";
+                break;
+            }
+        }
+        queue<int>q;
+        q.push(node);
+        unordered_map<int,int>vis;
+        while(!q.empty()){
+            int fr=q.front();
+            vis[fr]=1;
+            q.pop();
+            ans.push_back(fr);
+            for(auto it:mp[fr]){
+                // in[it]=in[it]-1;
+                // if(in[it]==1){
+                    if(!vis[it])
+                    q.push(it);
+                // }
+            }
+            in.erase(fr);
+        }
+        return ans;
+    }
+};
