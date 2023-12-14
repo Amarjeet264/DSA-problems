3050e28a0f07c0caf7163ba35261fc64a800335a
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2023-12-14 20:37:53 +0530

    Time: 68 ms (83.67%) | Memory: 46.8 MB (9.23%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -1,11 +1,29 @@
 #include<bits/stdc++.h>
 class Solution {
+    vector<int> nse(vector<int>&a){
+        vector<int>n;
+        stack<int>s;
+        for(int i=a.size()-1; i>=0; i--){
+            while(!s.empty() && a[s.top()]>=a[i]){
+                s.pop();
+            }
+            if(!s.empty()){
+                n.push_back(s.top());
+            }
+            else{
+                n.push_back(a.size());
+            }
+            s.push(i);
+        }
+        reverse(n.begin(), n.end());
+        return n;
+    }
     vector<int> pse(vector<int>&a){
         vector<int>p;
         int n = a.size();
         stack<int>s;
         for(int i=0; i<n; i++){
-            while(!s.empty() && a[s.top()]>=a[i]){
+            while(!s.empty() && a[s.top()]>a[i]){
                 s.pop();
             }
             if(!s.empty()){
@@ -21,19 +39,30 @@ class Solution {
 
 public:
     int sumSubarrayMins(vector<int>& arr) {
-        int m = 1e9+7;
-        vector<int>prevsmall=pse(arr);
-        int n=arr.size();
-        vector<int> ans(n,0);
-        for(int i = 0; i<n; i++){
-            
-            if(prevsmall[i] == -1) ans[i] = ((i+1)*arr[i])%m;
-            else ans[i]=(ans[prevsmall[i]]+(arr[i]*(i-prevsmall[i]))%m)%m;
-            
+        int mod=1e9+7;
+        long long total=0;
+        vector<int>p = pse(arr);
+        vector<int>n = nse(arr);
+        int m = arr.size();
+        long long int sum=0;
+        for(int i=0; i<m; i++){
+            int x = p[i];
+            int y = n[i];
+            if(x==-1){
+                x=i+1;
+            }
+            else{
+                x=i-p[i];
+            }
+            if(y==m){
+                y=m-i;
+            }
+            else{
+                y=n[i]-i;
+            }
+            total=(total%mod+(long long)arr[i]*(x*y))%mod;
         }
-        
-        int sum=0;
-        for(auto x:ans) sum=(sum+x)%m;
-        return sum;
+       return total%mod;
+
     }
 };
