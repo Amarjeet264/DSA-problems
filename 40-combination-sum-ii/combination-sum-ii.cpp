d3fd1ab9dbdf647967f52624895ffda926faf607
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2023-12-25 01:44:23 +0530

    Time: 9 ms (26.71%) | Memory: 11.1 MB (58.86%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+    vector<vector<int>>ans;
+    void comb(vector<int>&arr,int n,int target,int i,vector<int>&temp){
+        if(target==0){
+            ans.push_back(temp);
+            return;
+        }
+        if(i>=n){
+            return ;
+        }
+        for(int j=i;j<n;j++){
+            if(j>i&&arr[j]==arr[j-1]){
+                continue;
+            }
+            // if(temp.size()==0||(temp.size()>0&&arr[j]>=temp.back())){
+                else if(target>=arr[j]){
+                    temp.push_back(arr[j]);
+                    comb(arr,n,target-arr[j],j+1,temp);
+                    temp.pop_back();
+                }
+            // }
+        }
+    }
+public:
+    vector<vector<int>> combinationSum2(vector<int>& arr, int target) {
+        vector<int>temp;
+        int n=arr.size();
+        sort(arr.begin(),arr.end());
+        comb(arr,n,target,0,temp);
+        // for(int i=0;i<ans.size();i++){
+        //     for(int j=0;j<ans[i].size();j++){
+        //         cout<<ans[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        return ans;
+    }
+};
