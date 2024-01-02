03440f2eb9a6d32520f0f19ee9ef97b2a6adddbc
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-01-02 15:35:39 +0530

    Time: 6 ms (6.18%) | Memory: 6.9 MB (43.78%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,29 +1,42 @@
 
 class Solution {
-    void find(string digits,string s[],int i,vector<string>&ans,string &dup){
-        if(i>=digits.length()){
-            ans.push_back(dup);
-            return;
+    vector<string> ans;
+    void find(int i,string s, string &temp, unordered_map<int,vector<char>>&m){
+        if(i==s.size()){
+            if(temp.size()!=0)
+            ans.push_back(temp);
+            return ;
         }
-        int num=digits[i]-'0';
-        cout<<num<<" ";
-        string atnum=s[num];
-        for(int j=0;j<atnum.length();j++){
-            dup.push_back(atnum[j]);
-            find(digits,s,i+1,ans,dup);
-            dup.pop_back();
+        int num=s[i]-'0';
+        for(auto it:m[num]){
+            temp.push_back(it);
+            find(i+1,s,temp,m);
+            temp.pop_back();
         }
-
     }
-public:
+    public:
     vector<string> letterCombinations(string digits) {
-       string s[]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-       if(digits.length()==0){
-           return {};
-       }
-       vector<string>ans;
-       string dup="";
-       find(digits,s,0,ans,dup);
-       return ans;
+        unordered_map<int,vector<char>>m;
+        char ch='a';
+        int cnt=0;
+        int i=2;
+        while(i<=9){
+            m[i].push_back(ch);
+            ch++;
+            cnt++;
+            if(i!=7 && i!=9 && cnt==3){
+                cnt = 0;
+                i++;
+            }
+            if(i==7 || i==9){
+                if(cnt==4){
+                    cnt = 0;
+                    i++;
+                }
+            }
+        }
+        string temp="";
+        find(0,digits,temp,m);
+        return ans;
     }
 };
