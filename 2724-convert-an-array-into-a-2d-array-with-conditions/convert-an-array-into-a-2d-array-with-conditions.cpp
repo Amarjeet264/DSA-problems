59a5b5ce6cc07ffef72f16e3de2703aaebe67003
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-01-04 15:23:37 +0530

    Time: 8 ms (94.03%) | Memory: 30.5 MB (20.50%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.cpp
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -1,35 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> findMatrix(vector<int>& nums) {
-        vector<vector<int>> ans;
-        map<int, int> mp;
-        
-        // Count occurrences of each element
-        for (int i = 0; i < nums.size(); i++) {
+        vector<vector<int>>ans;
+        map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
             mp[nums[i]]++;
         }
-
-        while (!mp.empty()) {
-            vector<int> dup;
-            vector<int> elementsToRemove;
-
-            for (auto& it : mp) {
-                int ele = it.first;
+        while(mp.size()>0){
+            vector<int>dup;
+            vector<int>rem;
+            for(auto it:mp){
+                int ele=it.first;
                 dup.push_back(ele);
-                it.second--;
-
-                if (it.second == 0) {
-                    elementsToRemove.push_back(ele);
+                mp[ele]--;
+                if(mp[ele]==0){
+                    rem.push_back(ele);
                 }
             }
-
-            for (int ele : elementsToRemove) {
-                mp.erase(ele);
+            for(auto x:rem){
+                mp.erase(x);
             }
-
             ans.push_back(dup);
         }
-
         return ans;
     }
 };
