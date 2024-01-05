6643ea0a01a6dc8a6caca4e7e849c9fafedff77e
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-01-06 00:18:34 +0530

    Time: 43 ms (78.03%) | Memory: 42.8 MB (45.05%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
@@ -1,35 +1,29 @@
 class Solution {
-    // int maxii(int i,vector<int>&cards,int k,int j,vector<int>dp){
-    //     if(k<=0){
-    //         return 0;
-    //     }
-    //     if(i>j){
-    //         return 0;
-    //     }
-    //     if(dp[i][j]!=-1)return dp[i][j];
-    //     int x=cards[i]+maxii(i+1,cards,k-1,j,dp);
-    //     int y=cards[j]+maxii(i,cards,k-1,j-1,dp);
-    //     return dp[i][j]= max(x,y);
-    // }
 public:
-    int maxScore(vector<int>& cards, int k) {
-        int amount=0;
+    int maxScore(vector<int>& card, int k) {
+        int n=card.size();
+        int sum=0;
+        if(card.size()<=k){
+            for(int i=0;i<n;i++){
+                sum+=card[i];
+            }
+            return sum;
+        }
         int i=0;
-        int c=k;
-       while(c>=1&&i<cards.size()){
-           amount+=cards[i];
-           i++;
-           c--;
-       }
-       int maxi=amount;
-       int j=cards.size()-1;
-       i=k-1;
-       while(i>=0&&j>i){
-            amount=amount-cards[i]+cards[j];
-            maxi=max(maxi,amount);
-            j--;
+        while(i<k){
+            sum+=card[i];
+            i++;
+        }
+        i--;
+        int maxi=sum;
+        int j=card.size()-1;
+        while(k--){
+            sum-=card[i];
+            sum+=card[j];
             i--;
-       }
-       return maxi;
+            j--;
+            maxi=max(maxi,sum);
+        }
+        return maxi;
     }
 };
