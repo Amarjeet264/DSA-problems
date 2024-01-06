45cf74d21d3c21c51240abfdd90441def602560e
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-01-07 01:50:46 +0530

    Time: 69 ms (53.07%) | Memory: 30.5 MB (8.19%) - LeetSync

---
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& cards, int size) {
+        unordered_map<int,int>mp;
+        for(int i=0;i<cards.size();i++){
+            mp[cards[i]]++;
+        }
+        priority_queue<pair<int,int>>pq;
+        for(auto &it:mp){
+            pq.push({it.first,it.second});
+        }
+        while(!pq.empty()){
+            vector<pair<int,int>>dup;
+            int prev=-1;
+            int sz=size;
+            while(sz--){
+                int ele=pq.top().first;
+                int fre=pq.top().second;
+                // cout<<ele<<" "<<fre<<endl;
+                pq.pop();
+                if(prev==-1||abs(ele-prev)==1){
+                    prev=ele;
+                }
+                else{
+                    return false;
+                }
+                if(fre-1>0)dup.push_back({ele,fre-1});
+            }
+            // cout<<endl<<endl;
+            for(int i=0;i<dup.size();i++){
+                pq.push(dup[i]);
+            }
+        }
+        return true;
+    }
+};
