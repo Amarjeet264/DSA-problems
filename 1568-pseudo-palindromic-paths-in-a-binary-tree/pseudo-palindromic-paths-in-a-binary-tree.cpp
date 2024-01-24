74df3e0b9a9514d20f744b9f63d82ba1789e73d1
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-01-24 17:20:50 +0530

    Time: 231 ms (97.32%) | Memory: 177.8 MB (98.12%) - LeetSync

--- 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -10,26 +10,28 @@
  * };
  */
 class Solution {
-    int count(TreeNode* root,vector<int>vis){
+    int count(TreeNode* root,vector<int>&vis){
         if(!root){
             return 0;
         }
-        vis[root->val]++;
         if(!root->left&&!root->right){
+            vis[root->val]++;
             int cnt=0;
             for(int i=1;i<=9;i++){
                 if(vis[i]%2!=0){
                     cnt++;
                 }
             }
+            vis[root->val]--;
             if(cnt<=1){
                 return 1;
             }
             return 0;
         }
+        vis[root->val]++;
         int left=count(root->left,vis);
         int right=count(root->right,vis);
-
+        vis[root->val]--;
         return left+right;
     }
 public:
