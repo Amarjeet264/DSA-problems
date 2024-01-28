809c418c946a52d5c691a54052b5b10f2fcd6539
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-01-28 10:30:48 +0530

    Time: 152 ms (99.51%) | Memory: 11.6 MB (86.49%) - LeetSync

--- 1145-number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.cpp
+++ 1145-number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
     int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target) {
-        int ans=0;
-        int n=matrix.size();
-        int m=matrix[0].size();
-        for(int i=0;i<m;i++){
-            vector<int>pre(n,0);
-            for(int j=i;j<m;j++){
-                for(int k=0;k<n;k++){
-                    pre[k]+=matrix[k][j];
+        int ans = 0;
+        int n = matrix.size(), m = matrix[0].size();
+        for(int left = 0 ; left < m ; left++)
+        {
+            vector<int>pre(n, 0);
+            for(int right = left ; right < m ; right++)
+            {
+                for(int i = 0 ; i<n; i++)
+                {
+                    pre[i] += matrix[i][right];
                 }
-                for(int k=0;k<n;k++){
-                    int sum=0;
-                    for(int s=k;s<n;s++){
-                        sum+=pre[s];
-                        if(sum==target){
-                            ans+=1;
+
+                for(int i = 0 ; i<n;i++)
+                {
+                    int sum = 0;
+                    for(int j = i ; j<n; j++)
+                    {
+                        sum += pre[j];
+                        if(sum == target)
+                        {
+                            ans += 1;
                         }
                     }
                 }
