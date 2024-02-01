df40a08ca04e1cf2320eee9e178bca216675308c
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-02-01 11:07:33 +0530

    Time: 144 ms (90.31%) | Memory: 119.6 MB (39.25%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.cpp
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.cpp
@@ -3,7 +3,7 @@ public:
     vector<vector<int>> divideArray(vector<int>& nums, int k) {
       sort(nums.begin(),nums.end());
       vector<vector<int>>ans;
-    vector<int>dup;
+      vector<int>dup;
       for(int i=0;i<nums.size();i++){
          if(dup.size()==0)dup.push_back(nums[i]);
           else{
