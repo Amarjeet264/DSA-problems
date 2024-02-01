187c493b10d2ec1597a37b7c7964adfb4a97f671
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-02-01 23:43:40 +0530

    Time: 4 ms (85.13%) | Memory: 8.8 MB (27.76%) - LeetSync

--- 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.cpp
+++ 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.cpp
@@ -1,38 +1,21 @@
 class Solution {
-    bool isposs(int len,int cost,string s,string t){
+public:
+    int equalSubstring(string s, string t, int maxCost) {
         int i=0;
         int j=0;
-        int maxlen=0;
+        int sum=0;
+        int maxi=0;
         while(j<s.length()){
             if(s[j]!=t[j]){
-                cost-=abs(s[j]-t[j]);
-                if(cost<0){
-                    while(i<=j&&cost<0){
-                        cost+=abs(s[i]-t[i]);
-                        i++;
-                    }
-                }
+                sum+=abs(s[j]-t[j]);
             }
-            maxlen=max(maxlen,j-i+1);
+            while(sum>maxCost){
+                sum-=abs(s[i]-t[i]);
+                i++;
+            }
+            maxi=max(maxi,j-i+1);
             j++;
         }
-        return maxlen>=len;
-    }
-public:
-    int equalSubstring(string s, string t, int maxC) {
-        int low=0;
-        int high=s.length()+1;
-        int ans=0;
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(isposs(mid,maxC,s,t)){
-                ans=mid;
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
-            }
-        }
-        return ans;
+        return maxi;
     }
 };
