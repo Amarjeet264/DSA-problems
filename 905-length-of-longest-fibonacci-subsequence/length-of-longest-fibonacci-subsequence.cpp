a0da462bda849d102c641a79a4867b6266fe77bc
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-02-06 04:40:56 +0530

    Time: 489 ms (31.99%) | Memory: 149.4 MB (6.05%) - LeetSync

---
+++ 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.cpp
@@ -0,0 +1,34 @@
+class Solution {
+    unordered_map<int,int>mp;
+    int call(int prev,vector<int>&arr,int i,vector<vector<int>>&dp){
+        if(i>=arr.size()){
+            return 0;
+        }
+        if(dp[i][prev]!=-1){
+            return dp[i][prev];
+        }
+        // int nottake=call(prev,arr,i+1,dp);
+        int take=0;
+        if(mp.find(arr[i]+arr[prev])!=mp.end()){
+            take=1+call(i,arr,mp[arr[i]+arr[prev]],dp);
+        }
+        else{
+            return 0;
+        }
+        return dp[i][prev]=take;
+    }
+public:
+    int lenLongestFibSubseq(vector<int>& arr) {
+        for(int i=0;i<arr.size();i++){
+            mp[arr[i]]=i;
+        }
+        int maxi=0;
+        vector<vector<int>>dp(arr.size(),vector<int>(arr.size(),-1));
+        for(int i=0;i<arr.size();i++){
+           for(int j=i+1;j<arr.size();j++){
+                maxi=max(maxi,2+call(i,arr,j,dp));
+           }
+        }
+        return maxi<=2?0:maxi;
+    }
+};
