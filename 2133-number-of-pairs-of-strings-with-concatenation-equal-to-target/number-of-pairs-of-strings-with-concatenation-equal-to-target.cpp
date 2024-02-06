4230f1ad5c3ac9598d4c0a7eed4f444fce5bd93c
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-02-06 12:50:55 +0530

    Time: 4 ms (98.24%) | Memory: 14.3 MB (76.90%) - LeetSync

--- 2133-number-of-pairs-of-strings-with-concatenation-equal-to-target/number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
+++ 2133-number-of-pairs-of-strings-with-concatenation-equal-to-target/number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public:
     int numOfPairs(vector<string>& nums, string target) {
+        unordered_map<string,int>mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
         int cnt=0;
         for(int i=0;i<nums.size();i++){
-            for(int j=0;j<nums.size();j++){
-                if(nums[i]+nums[j]==target&&i!=j){
-                    cnt++;
-                }
+            string x="";
+            if(target.substr(0,nums[i].size())==nums[i]&&nums[i].size()<target.size())x=target.substr(nums[i].size(),target.size()-nums[i].size());
+            // cout<<nums[i]<<" "<<x<<endl;
+            if(x==nums[i]){
+                cnt+=mp[x]-1;
             }
+            else if(x!=""){
+                cnt+=mp[x];
+            }
+            cout<<cnt<<endl;
         }
         return cnt;
     }
