c6e5f8121c5941fccd9877750b0e051c497203dc
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-02-07 00:47:33 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (31.84%) - LeetSync

---
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    void jodo(string &s){
+        int i=s.length()-1;
+        while(i>=0&&s[i]=='1'){
+            s[i]='0';
+            i--;
+        }
+        if(i>0)s[i]='1';
+        else{
+            reverse(s.begin(),s.end());
+            s+='1';
+            reverse(s.begin(),s.end());
+        }
+    }
+public:
+    int numSteps(string s) {
+        int n=s.length();
+        int i=n-1;
+        int cnt=0;
+        while(s.length()>1){
+            if(s[s.size()-1]=='0'){
+                s.pop_back();
+            }
+            else{
+                jodo(s);
+            }
+            cnt++;
+        }
+        return cnt;
+    }
+};
