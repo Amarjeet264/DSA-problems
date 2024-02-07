be0224d6e10dbe9f9b839f46c0ce7add6b36140c
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-02-07 16:47:40 +0530

    Time: 9 ms (90.76%) | Memory: 11.8 MB (47.06%) - LeetSync

---
+++ 794-swim-in-rising-water/swim-in-rising-water.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    int dr[4]={-1,0,1,0};
+    int dc[4]={0,1,0,-1};
+public:
+    int swimInWater(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        queue<pair<int,int>>q;
+        vector<vector<int>>till(n,vector<int>(m,INT_MAX));
+        q.push({0,0});
+        till[0][0]=grid[0][0];
+        while(!q.empty()){
+            int row=q.front().first;
+            int col=q.front().second;
+            q.pop();
+            for(int k=0;k<4;k++){
+                int nrow=row+dr[k];
+                int ncol=col+dc[k];
+                if(nrow>=0&&nrow<n&&ncol>=0&&ncol<m&&till[nrow][ncol]>max(grid[nrow][ncol],till[row][col])){
+                    till[nrow][ncol]=max(till[row][col],grid[nrow][ncol]);
+                    q.push({nrow,ncol});
+                }
+            }
+        }
+        return till[n-1][m-1];
+    }
+};
