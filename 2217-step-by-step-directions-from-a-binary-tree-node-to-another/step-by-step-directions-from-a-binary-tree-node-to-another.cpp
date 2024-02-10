60493ceb7d219957d5deabf801b6822fe1c52dcc
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-02-10 16:55:04 +0530

    Time: 136 ms (97.28%) | Memory: 117.2 MB (54.53%) - LeetSync

---
+++ 2217-step-by-step-directions-from-a-binary-tree-node-to-another/step-by-step-directions-from-a-binary-tree-node-to-another.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+   bool findPath(TreeNode* root, int tar, string& path){
+
+        if(root == nullptr) return false;
+        if(root->val == tar) return true;
+
+        bool left = findPath(root->left,tar,path);
+        if(left){
+            path += 'L';
+            return true;
+        }
+        bool right = findPath(root->right,tar,path);
+        if(right){
+            path += 'R';
+            return true;
+        }
+        return false;
+    }
+public:
+    string getDirections(TreeNode* root, int startValue, int destValue) {
+        string temp="";
+
+        findPath(root,startValue,temp);
+
+        string temp1="";
+        findPath(root,destValue,temp1);
+        // cout<<temp<<" "<<temp1<<" ";
+        while(temp.size()>0&&temp1.size()>0&&temp[temp.size()-1]==temp1[temp1.size()-1]){
+            temp.pop_back();
+            temp1.pop_back();
+        }
+        // reverse(temp.begin(),temp.end());
+        reverse(temp1.begin(),temp1.end());
+        string ans="";
+        while(ans.size()<temp.size()){
+            ans+='U';
+        }
+        // cout<<root1->val;
+        return ans+temp1;
+    }
+};
