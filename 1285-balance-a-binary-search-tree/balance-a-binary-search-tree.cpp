760e850634e2504d45b7a66e9460dfccbab1fdf1
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-02-24 23:28:30 +0530

    Time: 196 ms (26.12%) | Memory: 62.7 MB (61.29%) - LeetSync

---
+++ 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    vector<int>vec;
+    void balance(TreeNode* root){
+        if(!root){
+            return;
+        }
+        balance(root->left);
+        vec.push_back(root->val);
+        balance(root->right);
+    }
+    TreeNode* maketree(int i,int j){
+        if(i>j){
+            // if(i==j){
+            //     TreeNode* root=new TreeNode(vec[i]);
+            //     return root;
+            // }
+            return nullptr;
+        }
+        int mid=(i+j)/2;
+        TreeNode* root=new TreeNode(vec[mid]);
+        cout<<i<<" "<<j<<" "<<mid<<" "<<root->val<<endl;
+        root->left=maketree(i,mid-1);
+        root->right=maketree(mid+1,j);
+        return root;
+    }
+public:
+    TreeNode* balanceBST(TreeNode* root) {
+        balance(root);
+        // for(int i=0;i<vec.size();i++){
+        //     cout<<vec[i]<<" ";
+        // }
+        TreeNode* newroot=maketree(0,vec.size()-1);
+        return newroot;
+    }
+};
