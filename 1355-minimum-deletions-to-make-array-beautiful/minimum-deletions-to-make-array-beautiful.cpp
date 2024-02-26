26548fbf7e7c92a008ddc234ba59fbdace2a6a54
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-02-27 01:22:50 +0530

    Time: 143 ms (36.21%) | Memory: 123.6 MB (29.31%) - LeetSync

--- 1355-minimum-deletions-to-make-array-beautiful/minimum-deletions-to-make-array-beautiful.cpp
+++ 1355-minimum-deletions-to-make-array-beautiful/minimum-deletions-to-make-array-beautiful.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     int minDeletion(vector<int>& nums) {
-        vector<int>ans;
         int cnt=0;
         for(int i=0;i<nums.size()-1;i++){
             if((i-cnt)%2==0&&nums[i]==nums[i+1]){
                 // cout<<i<<" ";
                 cnt++;
-            }
-            else{
-                ans.push_back(nums[i]);
             }
         }
         if((nums.size()-cnt)%2!=0){
