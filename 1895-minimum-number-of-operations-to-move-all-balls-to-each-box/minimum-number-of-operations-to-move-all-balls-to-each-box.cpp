66dc39f38f56477196eb008d90a85ceca3f263a6
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-03-01 03:03:42 +0530

    Time: 4 ms (88.86%) | Memory: 10.6 MB (17.63%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -2,27 +2,27 @@ class Solution {
 public:
     vector<int> minOperations(string boxes) {
         vector<int>ans;
-        for(int i=0;i<boxes.size();i++){
+        // for(int i=0;i<boxes.size();i++){
             int final=0;
             int cnt=0;
-            for(int j=i+1;j<boxes.size();j++){
-                cnt++;
+            for(int j=0;j<boxes.size();j++){
+                ans.push_back(final);
                 if(boxes[j]=='1'){
-                    final+=cnt;
+                    cnt++;
                 }
+                    final+=cnt;
             }
-            // if(i==3){
-            //     cout<<final<<" "<<cnt<<;
-            // }
             cnt=0;
-            for(int j=i-1;j>=0;j--){
-                cnt++;
+            final=0;
+            for(int j=boxes.size()-1;j>=0;j--){
+                ans[j]+=final;
                 if(boxes[j]=='1'){
-                    final+=cnt;
+                    cnt++;
                 }
+                final+=cnt;
             }
-            ans.push_back(final);
-        }
+            // ans.push_back(final);
+        // }
         return ans;
     }
 };
