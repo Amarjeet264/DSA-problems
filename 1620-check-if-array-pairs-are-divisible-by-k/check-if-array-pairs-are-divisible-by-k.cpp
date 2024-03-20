a8269813a73d9e443960e8ff47c39c7df8e4c90d
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-03-20 13:43:24 +0530

    Time: 134 ms (44.23%) | Memory: 74.6 MB (28.92%) - LeetSync

--- 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.cpp
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
     bool canArrange(vector<int>& arr, int k) {
-        unordered_map<int,int> mp;
-        for(int i = 0; i < arr.size(); ++i) {
-            int remainder = arr[i] % k;
-            if (remainder < 0) {
-                remainder += k; // Ensure remainder is positive
+        unordered_map<int,int>mp;
+        for(int i=0;i<arr.size();i++){
+            // int x=(k-abs(arr[i])%k)%k;
+            int z=arr[i]%k;
+            if(z<0){
+                z+=k;
             }
-            int complement = (k - remainder) % k; // Find complement
-            if(mp.find(complement) != mp.end()) {
-                mp[complement]--;
-                if(mp[complement] == 0) {
-                    mp.erase(complement);
+            int x=(k-z)%k;
+            if(mp.find(x)!=mp.end()){
+                // cout<<i<<" ";
+                mp[x]--;
+                if(mp[x]==0){
+                    mp.erase(x);
                 }
-            } else {
-                mp[remainder]++;
+            }
+            else{
+                mp[z]++;
             }
         }
-        return mp.empty();
+        // cout<<(-2%3);
+        return mp.size()==0;
     }
 };
