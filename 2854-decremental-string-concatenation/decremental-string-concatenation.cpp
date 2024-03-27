814cb3e1215a06ddaae1c865b486a0de12d5de5c
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-03-27 12:38:55 +0530

    Time: 245 ms (30.29%) | Memory: 245.8 MB (6.86%) - LeetSync

--- 2854-decremental-string-concatenation/decremental-string-concatenation.cpp
+++ 2854-decremental-string-concatenation/decremental-string-concatenation.cpp
@@ -1,29 +1,31 @@
 class Solution {
-    int call(vector<vector<vector<int>>>& dp, vector<string>& words, int first, int last, int i) {
-        if (i >= words.size()) {
+    int call(vector<vector<vector<int>>>&dp,vector<string>&words,int first,int last,int i){
+        if(i>=words.size()){
             return 0;
         }
-        if (dp[i][first][last] != -1) {
+        if(dp[i][first][last]!=-1){
             return dp[i][first][last];
         }
-        string curr = words[i];
-        int fr = 0;
-        int la = 0;
-        if (last == curr[0]-'a') {
-            fr = curr.size() - 1 + call(dp, words, first, curr[curr.size() - 1]-'a', i + 1);
-        } else {
-            fr = curr.size() + call(dp, words, first, curr[curr.size() - 1]-'a', i + 1);
+        string curr=words[i];
+        int fr=0;
+        int la=0;
+        if(last==curr[0]-'a'){
+            fr=words[i].size()-1+call(dp,words,first,curr[curr.size()-1]-'a',i+1);
         }
-        if (curr[curr.size() - 1]-'a' == first) {
-            la = curr.size() - 1 + call(dp, words, curr[0]-'a', last, i + 1);
-        } else {
-            la = curr.size() + call(dp, words, curr[0]-'a', last, i + 1);
+        else{
+            fr=words[i].size()+call(dp,words,first,curr[curr.size()-1]-'a',i+1);
         }
-        return dp[i][first][last] = min(la, fr);
+        if(curr[curr.size()-1]-'a'==first){
+            la=curr.size()-1+call(dp,words,curr[0]-'a',last,i+1);
+        }
+        else{
+            la=curr.size()+call(dp,words,curr[0]-'a',last,i+1);
+        }
+        return dp[i][first][last]= min(la,fr);
     }
 public:
     int minimizeConcatenatedLength(vector<string>& words) {
-        vector<vector<vector<int>>> dp(words.size(), vector<vector<int>>(26, vector<int>(26, -1)));
-        return words[0].size() + call(dp, words, words[0][0]-'a', words[0][words[0].size() - 1]-'a', 1);
+        vector<vector<vector<int>>>dp(words.size(),vector<vector<int>>(26,vector<int>(26,-1)));
+        return words[0].size()+call(dp,words,words[0][0]-'a',words[0][words[0].size()-1]-'a',1);
     }
 };
