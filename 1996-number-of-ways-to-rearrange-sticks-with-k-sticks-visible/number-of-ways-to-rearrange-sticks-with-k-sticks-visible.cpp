c8c0e1589b2bd50397759feddf65f3e009c8c6ca
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-04-03 01:13:28 +0530

    Time: 104 ms (15.23%) | Memory: 11.6 MB (86.09%) - LeetSync

---
+++ 1996-number-of-ways-to-rearrange-sticks-with-k-sticks-visible/number-of-ways-to-rearrange-sticks-with-k-sticks-visible.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    // unordered_map<int,int>mp;
+    int mod=1e9+7;
+    int dp[1005][1005];
+    long long rearrange(int n,int k){
+        if(n==0){
+            return k==0;
+        }
+        if(k<0)return 0;
+        if(dp[n][k]!=-1){
+            return dp[n][k];
+        }
+        long long ans=rearrange(n-1,k-1)+((n-1)*rearrange(n-1,k)%mod);
+        ans=ans%mod;
+        // ans=(ans+((n-1)*rearrange(n-1,k)%mod)%mod);
+        // ans=ans%mod;
+        return dp[n][k]= ans;
+    }
+public:
+    int rearrangeSticks(int n, int k) {
+        memset(dp,-1,sizeof(dp));
+        return rearrange(n,k);
+    }
+};
