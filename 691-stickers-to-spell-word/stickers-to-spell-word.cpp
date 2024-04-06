9473c882beaf58f36b29281a9c59d31c25dd071e
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-04-06 14:38:51 +0530

    Time: 33 ms (82.60%) | Memory: 16.3 MB (68.76%) - LeetSync

---
+++ 691-stickers-to-spell-word/stickers-to-spell-word.cpp
@@ -0,0 +1,39 @@
+class Solution {
+    int stick(vector<vector<int>>&vec,map<string,int>&dp,string tar){
+        if(tar==""){
+            return 0;
+        }
+        if(dp.find(tar)!=dp.end()){
+            return dp[tar];
+        }
+        vector<int>freq(26);
+        int take=1e9;
+        for(int i=0;i<vec.size();i++){
+            if(vec[i][tar[0]-'a']==0)continue;
+            freq=vec[i];
+            string rem="";
+            for(int k=0;k<tar.size();k++){
+                if(freq[tar[k]-'a']>0){
+                    freq[tar[k]-'a']--;
+                }
+                else{
+                    rem+=tar[k];
+                }
+            }
+            take=min(take,1+stick(vec,dp,rem));
+        }
+        return dp[tar]= take;
+    }
+public:
+    int minStickers(vector<string>& stickers, string target) {
+        vector<vector<int>>vec(stickers.size(),vector<int>(26,0));
+        for(int i=0;i<stickers.size();i++){
+            for(int j=0;j<stickers[i].size();j++){
+                vec[i][stickers[i][j]-'a']++;
+            }
+        }
+        map<string,int>dp;
+        int x=stick(vec,dp,target);
+        return x>=1e5?-1:x;
+    }
+};
