e1748e5fea978159ee1cf5a26688d3526d93637d
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-04-06 14:40:41 +0530

    Time: 728 ms (15.70%) | Memory: 27.3 MB (45.84%) - LeetSync

--- 691-stickers-to-spell-word/stickers-to-spell-word.cpp
+++ 691-stickers-to-spell-word/stickers-to-spell-word.cpp
@@ -9,17 +9,20 @@ class Solution {
         vector<int>freq(26);
         int take=1e9;
         for(int i=0;i<vec.size();i++){
-            if(vec[i][tar[0]-'a']==0)continue;
+            // if(vec[i][tar[0]-'a']==0)continue;
+            bool ha=false;
             freq=vec[i];
             string rem="";
             for(int k=0;k<tar.size();k++){
                 if(freq[tar[k]-'a']>0){
                     freq[tar[k]-'a']--;
+                    ha=true;
                 }
                 else{
                     rem+=tar[k];
                 }
             }
+            if(!ha)continue;
             take=min(take,1+stick(vec,dp,rem));
         }
         return dp[tar]= take;
