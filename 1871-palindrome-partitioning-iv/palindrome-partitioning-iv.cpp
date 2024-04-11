a40e11a2db24732bd85d869c0f715a91bf62ea84
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-04-11 21:47:50 +0530

    Time: 2130 ms (21.81%) | Memory: 11.2 MB (82.21%) - LeetSync

---
+++ 1871-palindrome-partitioning-iv/palindrome-partitioning-iv.cpp
@@ -0,0 +1,41 @@
+class Solution {
+    bool ispalin(string &s,int i,int j){
+        while(i<j){
+            if(s[i]!=s[j]){
+                return 0;
+            }
+            i++;
+            j--;
+        }
+        return 1;
+    }
+    int palin(string &s,int i,int k,vector<vector<int>>&dp){
+        if(i>=s.size()){
+            if(k>0)return 1e5;
+            return 0;
+        }
+        if(k==0){
+            return 1e5;
+        }
+        if(dp[i][k]!=-1){
+            return dp[i][k];
+        }
+        int take=1e5;
+        for(int j=i;j<s.size();j++){
+            bool cnt=ispalin(s,i,j);
+            if(cnt){
+                take=min(take,palin(s,j+1,k-1,dp));
+            }
+        }
+        return dp[i][k]= take;
+    }
+public:
+    bool checkPartitioning(string s) {
+        int k=3;
+        vector<vector<int>>dp(s.size(),vector<int>(k+1,-1));
+        int x= palin(s,0,k,dp);
+        if(x>=1e5)return 0;
+        return 1;
+    }
+};
+
