ec1feb469cf88618bf77ba58cb08b4ee28a6df33
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-04-27 14:31:25 +0530

    Time: 19 ms (36.23%) | Memory: 26.2 MB (26.55%) - LeetSync

---
+++ 1207-delete-nodes-and-return-forest/delete-nodes-and-return-forest.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    vector<TreeNode*>ans;
+    void call(TreeNode* &root,unordered_set<int>&st){
+        if(!root){
+            return;
+        }
+        call(root->left,st);
+        call(root->right,st);
+        if(st.find(root->val)!=st.end()){
+            if(root->left)ans.push_back(root->left);
+            if(root->right)ans.push_back(root->right);
+            root->left=nullptr;
+            root->right=nullptr;
+            delete(root);
+            root=nullptr;
+            // return;
+        }
+    }
+public:
+    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
+        unordered_set<int>st;
+        for(auto it:to_delete){
+            st.insert(it);
+        }
+        call(root,st);
+        if(root)ans.push_back(root);
+        return ans;
+    }
+};
