38b308d33db5e6ecb94e4671dc98b3b0e5229ede
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-05-26 11:48:30 +0530

    Time: 1357 ms (13.04%) | Memory: 396.9 MB (41.15%) - LeetSync

---
+++ 552-student-attendance-record-ii/student-attendance-record-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    int mod=1e9+7;
+    int check(int n,int ab,int late,vector<vector<vector<int>>>&dp){
+        if(n<=0){
+            if(ab>=0&&late<3){
+                return 1;
+            }
+            return 0;
+        }
+        if(ab<0||late>=3){
+            return 0;
+        }
+        if(dp[n][ab][late]!=-1){
+            return dp[n][ab][late];
+        }
+        int pre=check(n-1,ab,0,dp);
+        int abs=check(n-1,ab-1,0,dp);
+        int lat=check(n-1,ab,late+1,dp);
+        int x=(pre+abs)%mod;
+        x=(x+lat)%mod;
+        return dp[n][ab][late]=x;
+    }
+public:
+    int checkRecord(int n) {
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(4,-1)));
+        int late=0;
+        return check(n,1,late,dp);
+    }
+};
