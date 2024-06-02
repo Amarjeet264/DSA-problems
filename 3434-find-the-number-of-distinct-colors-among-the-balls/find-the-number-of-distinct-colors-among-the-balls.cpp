4ec0d3e18d3f267d0df8a84dd34457b869995535
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-06-02 19:59:13 +0530

    Time: 372 ms (65.19%) | Memory: 155 MB (96.46%) - LeetSync

--- 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.cpp
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.cpp
@@ -8,24 +8,15 @@ public:
         for(int i=0;i<queries.size();i++){
             int ballno=queries[i][0];
             int color=queries[i][1];
-            // if(ball.find(ballno)==ball.end()){
-            //     if(col.find(color)==col.end()){
-            //         count++;
-            //     }
-            //     ball[ballno]=color;
-            //     col[color]++;
-            // }
-            // else{
-                int prevcolorofball=ball[ballno];
-                col[prevcolorofball]--;
-                if(col[prevcolorofball]==0){
-                    count--;
-                    col.erase(prevcolorofball);
-                }
-                if(col.find(color)==col.end())count++;
-                ball[ballno]=color;
-                col[color]++;
-            // }
+            int prevcolorofball=ball[ballno];
+            col[prevcolorofball]--;
+            if(col[prevcolorofball]==0){
+                count--;
+                col.erase(prevcolorofball);
+            }
+            if(col.find(color)==col.end())count++;
+            ball[ballno]=color;
+            col[color]++;
             ans[i]=count;
         }
         return ans;
