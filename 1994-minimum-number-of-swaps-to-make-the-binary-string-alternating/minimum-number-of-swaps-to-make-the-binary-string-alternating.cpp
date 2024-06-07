1b42ee7b96ffd19273c36b10a6e40f1e30811796
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-06-07 19:00:23 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (27.50%) - LeetSync

--- 1994-minimum-number-of-swaps-to-make-the-binary-string-alternating/minimum-number-of-swaps-to-make-the-binary-string-alternating.cpp
+++ 1994-minimum-number-of-swaps-to-make-the-binary-string-alternating/minimum-number-of-swaps-to-make-the-binary-string-alternating.cpp
@@ -1,4 +1,34 @@
 class Solution {
+    int place1(string s){
+        int i=0;
+        bool one=true;
+        int cnt=0;
+        while(i<s.size()){
+            if(one){
+                if(s[i]!='1'){
+                    cnt++;
+                }
+            }
+            one=!one;
+            i++;
+        }
+        return cnt;
+    }
+    int place0(string s){
+        int i=0;
+        int cnt=0;
+        bool zero=true;
+        while(i<s.size()){
+            if(zero){
+                if(s[i]!='0'){
+                    cnt++;
+                }
+            }
+            zero=!zero;
+            i++;
+        }
+        return cnt;
+    }
 public:
     int minSwaps(string s) {
         int cnt1=0;
@@ -14,67 +44,12 @@ public:
         if(abs(cnt0-cnt1)>1){
             return -1;
         }
-        int cnt=0;
-        // cout<<cnt0<<" "<<cnt1<<endl;
         if(cnt0>cnt1){
-            int i=0;
-            bool zero=true;
-            while(i<s.size()){
-                if(zero){
-                    if(s[i]!='0'){
-                        cnt++;
-                    }
-                }
-                zero=!zero;
-                i++;
-            }
+            return place0(s);
         }
         else if(cnt1>cnt0){
-            int i=0;
-            bool one=true;
-            while(i<s.size()){
-                if(one){
-                    if(s[i]!='1'){
-                        cnt++;
-                    }
-                }
-                one=!one;
-                i++;
-            }
+            return place1(s);
         }
-        else{
-                int mini=0;
-                int i=0;
-                bool zero=true;
-                while(i<s.size()){
-                    if(zero){
-                        if(s[i]!='0'){
-                            cnt++;
-                        }
-                    }
-                    zero=!zero;
-                    i++;
-                }
-                cout<<cnt<<" ";
-                mini=cnt;
-                cnt=0;
-    
-                i=0;
-                bool one=true;
-                while(i<s.size()){
-                    if(one){
-                        if(s[i]!='1'){
-                            cnt++;
-                        }
-                    }
-                    one=!one;
-                    i++;
-                }
-                if(cnt<mini){
-                    mini=cnt;
-                }
-                return mini;
-        }
-        return cnt;
+        return min(place1(s),place0(s));
     }
 };
