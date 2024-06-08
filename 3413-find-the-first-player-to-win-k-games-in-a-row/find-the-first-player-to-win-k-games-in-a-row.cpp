06919812f51f2c61ddaea4698e403333876109d5
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-06-08 23:49:25 +0530

    Time: 187 ms (75.00%) | Memory: 119 MB (37.50%) - LeetSync

--- 3413-find-the-first-player-to-win-k-games-in-a-row/find-the-first-player-to-win-k-games-in-a-row.cpp
+++ 3413-find-the-first-player-to-win-k-games-in-a-row/find-the-first-player-to-win-k-games-in-a-row.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     int findWinningPlayer(vector<int>& skills, int k) {
-        deque<int>dq;
-        for(int i=0;i<skills.size();i++){
-            dq.push_back(i);
-        }
-        bool winner=true;
-        unordered_map<int,int>mp;
         if(k>=skills.size()){
             int maxi=0;
             for(int i=1;i<skills.size();i++){
@@ -16,28 +10,20 @@ public:
             }
             return maxi;
         }
-        while(winner){
-            int x=dq.front();
-            dq.pop_front();
-            int y=dq.front();
-            dq.pop_front();
-            if(skills[x]>skills[y]){
-                mp[x]++;
-                dq.push_front(x);
-                dq.push_back(y);
-                if(mp[x]==k){
-                    return x;
-                }
+        int winner=0;
+        int cnt=0;
+        for(int i=1;i<skills.size();i++){
+            if(skills[i]<skills[winner]){
+                cnt++;
             }
             else{
-                mp[y]++;
-                if(mp[y]==k){
-                    return y;
-                }
-                dq.push_front(y);
-                dq.push_back(x);
+                winner=i;
+                cnt=1;
+            }
+            if(cnt==k){
+                return winner;
             }
         }
-        return -1;
+        return winner;
     }
 };
