e785f3bdbd783ccbdaacfab7e5d79d983eaec6f0
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-06-09 14:18:19 +0530

    Time: 208 ms (21.49%) | Memory: 134.7 MB (59.63%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.cpp
+++ 523-continuous-subarray-sum/continuous-subarray-sum.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
-    static bool checkSubarraySum(vector<int>& nums, int k) {
-        int n=nums.size();
-        if (n<2) return 0; //its length is at least two
-        unordered_map<int, vector<int>> mod_k;// 1 <= k <= 2^31 - 1
-        int prefix=0;
-        mod_k[0].push_back(-1);//sum can begin from index i=0
-        for(int i=0; i<n; i++){
-            prefix+=nums[i];
-            prefix%=k;
-            if (mod_k.count(prefix) && i>mod_k[prefix].front()+1) 
-                return 1;
-            mod_k[prefix].push_back(i);// push index i to mod_k[prefix]
-        }
-        return 0;
+    static bool checkSubarraySum(vector<int>&arr, int K) {
+        int N=arr.size();
+        unordered_map<int,int>mp;
+	    int rem=0;
+	    mp[0]=-1;
+	    int sum=0;
+	    int cnt=0;
+	    for(int i=0;i<N;i++){
+	        sum+=arr[i];
+            // if(sum==0)continue;
+	        rem=sum%K;
+	        if(rem<0){
+	            rem=rem+K;
+	        }
+	        if(mp.find(rem)!=mp.end()){
+                if(i-mp[rem]>1)
+	               return true;
+	        }
+	        if(mp.find(rem)==mp.end())mp[rem]=i;
+	    }
+	    return false;
     }
       
 };
