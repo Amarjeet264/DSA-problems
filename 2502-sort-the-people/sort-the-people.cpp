5e341932297b04397d2a72e08d194581512962ef
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-06-27 15:38:43 +0530

    Time: 28 ms (63.65%) | Memory: 29.9 MB (16.63%) - LeetSync

---
+++ 2502-sort-the-people/sort-the-people.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+    vector<string> ans;
+    
+    unordered_map<int,string> mpp;
+    
+    for(int i=0;i<names.size();i++){
+        mpp[heights[i]]=names[i];
+    }
+    
+    sort(heights.rbegin(),heights.rend());
+    
+    for(int i=0;i<heights.size();i++){
+        ans.push_back(mpp[heights[i]]);
+    }
+    
+    return ans;
+}
+};
