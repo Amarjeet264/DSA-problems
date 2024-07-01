20a6d558fb9a16a1dcb48ed9ec0d4b1007a8819f
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-07-01 18:39:57 +0530

    Time: 3 ms (69.34%) | Memory: 11.6 MB (58.44%) - LeetSync

--- 797-rabbits-in-forest/rabbits-in-forest.cpp
+++ 797-rabbits-in-forest/rabbits-in-forest.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     int numRabbits(vector<int>& answers) {
-        int count = 0;
-        // Initialize map
-        int n=answers.size();
-        unordered_map<int, int> mp;
-        for (int i = 0; i < n; ++i) {
-    
-            // Add to the count if not found or
-            // has exhausted the count of all the
-            // number of rabbits of same colour
-            if (mp[answers[i]] == 0) {
-                count += answers[i] + 1;
-                mp[answers[i]] = answers[i] + 1;
-            }
-            mp[answers[i]]--;
+        unordered_map<int,int>mp;
+        for(int i=0;i<answers.size();i++){
+            mp[answers[i]]++;
         }
-    
-        // count gives minimum number
-        // of rabbits in the forest
-        return count;
+        int cnt=0;
+        for(auto it:mp){
+            int rem = it.second%(it.first+1);
+            int g = it.second/(it.first+1);
+            cnt+=g*(it.first+1);
+            if(rem!=0){
+                cnt+=(it.first+1);
+            }
+        }
+        return cnt;
     }
 };
