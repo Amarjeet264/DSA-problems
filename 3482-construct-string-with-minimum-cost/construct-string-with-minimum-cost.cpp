7a116807221d6fd7714c5ccbbde4fc05b84a9ce9
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-07-08 10:33:50 +0530

    Time: 611 ms (50.00%) | Memory: 495.1 MB (25.00%) - LeetSync

--- 3482-construct-string-with-minimum-cost/construct-string-with-minimum-cost.cpp
+++ 3482-construct-string-with-minimum-cost/construct-string-with-minimum-cost.cpp
@@ -1,7 +1,19 @@
 class TrieNode {
 public:
-    unordered_map<char, TrieNode*> children;
+    TrieNode* children[26];
     long long cost=LLONG_MAX;
+
+    void put(char c, TrieNode* node) {
+        children[c - 'a'] = node;
+    }
+
+    TrieNode* get(char c) {
+        return children[c - 'a'];
+    }
+
+    bool containsKey(char c) {
+        return children[c - 'a'] != nullptr;
+    }
 };
 
 class Solution {
@@ -10,10 +22,10 @@ class Solution {
     void insert(string& word, long long cost) {
         TrieNode* node = root;
         for (char c : word) {
-            if (node->children.find(c) == node->children.end()) {
-                node->children[c] = new TrieNode();
+            if (!node->containsKey(c)) {
+                node->put(c, new TrieNode());
             }
-            node = node->children[c];
+            node = node->get(c);
         }
         node->cost = min(node->cost, cost); // Update the cost to the minimum one
     }
@@ -29,8 +41,8 @@ class Solution {
         long long mini = 1e12;
         TrieNode* node = root;
         for (int j = i; j < target.size(); j++) {
-            if (node->children.find(target[j]) == node->children.end()) break;
-            node = node->children[target[j]];
+            if (!node->containsKey(target[j])) break;
+            node = node->get(target[j]);
             if (node->cost != LLONG_MAX) {
                 long long take = node->cost + call(target, j + 1, dp);
                 mini = min(mini, take);
