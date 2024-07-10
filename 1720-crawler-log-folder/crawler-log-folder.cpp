defb68976a98540d39f0affb1612bcb97e13c2af
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-07-10 11:42:07 +0530

    Time: 4 ms (67.13%) | Memory: 14 MB (61.11%) - LeetSync

---
+++ 1720-crawler-log-folder/crawler-log-folder.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int cnt = 0;
+        for(int i=0;i<logs.size();i++){
+            if(logs[i][0]!='.'){
+                cnt++;
+            }
+            else if(logs[i][1]=='.'&&cnt>0){
+                cnt--;
+            }
+        }
+        return cnt>=0?cnt:0;
+    }
+};
