557ce90fd459a73bec27242330499a702dfd5566
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-07-11 11:25:43 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (8.88%) - LeetSync

--- 3192-maximum-xor-product/maximum-xor-product.cpp
+++ 3192-maximum-xor-product/maximum-xor-product.cpp
@@ -1,42 +1,35 @@
-#include <iostream>
-using namespace std;
-
 class Solution {
+
 public:
-    typedef long long ll;
+   typedef long long ll;
     int maximumXorProduct(long long a, long long b, int n) {
         long long xXora = 0;
         long long xXorb = 0;
-        
-        // Iterate through the bits from the most significant to the least significant
-        for (int i = 49; i >= n; --i) {
-            bool bit_a = (a >> i) & 1;
-            bool bit_b = (b >> i) & 1;
-            xXora = (xXora << 1) | bit_a;
-            xXorb = (xXorb << 1) | bit_b;
+        for(ll i=49;i>=n;i--){
+            bool bit_a = ((a>>i)&1)>0;
+            bool bit_b = ((b>>i)&1)>0;
+            if(bit_a)xXora = (xXora^(1ll<<i));
+            if(bit_b)xXorb = (xXorb^(1ll<<i));
         }
-        
-        for (int i = n-1; i >= 0; --i) {
-            bool bit_a = (a >> i) & 1;
-            bool bit_b = (b >> i) & 1;
-            
-            if (bit_a == bit_b) {
-                xXora = (xXora << 1) | 1;
-                xXorb = (xXorb << 1) | 1;
-            } else {
-                if (xXora > xXorb) {
-                    xXorb = (xXorb << 1) | 1;
-                    xXora = (xXora << 1);
-                } else {
-                    xXora = (xXora << 1) | 1;
-                    xXorb = (xXorb << 1);
+        for(ll i=n-1;i>=0;i--){
+            bool bit_a = (a>>i)&1;
+            bool bit_b = (b>>i)&1;
+            if(bit_a==bit_b){
+                xXora ^= (1ll<<i);
+                xXorb ^= (1ll<<i);
+            }
+            else{
+                if(xXora>xXorb){
+                    xXorb ^= (1ll<<i);
+                }
+                else{
+                    xXora ^= (1ll<<i);
                 }
             }
         }
-        
-        int mod = 1e9 + 7;
-        xXora %= mod;
-        xXorb %= mod;
-        return (xXora * xXorb) % mod;
+        int mod = 1e9+7;
+        xXora%=mod;
+        xXorb%=mod;
+        return (xXora*xXorb)%mod;
     }
 };
