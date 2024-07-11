120ff7086d50d63dd4c4a0831c585a89c40be82c
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-07-11 09:41:52 +0530

    Time: 72 ms (75.82%) | Memory: 81.6 MB (20.01%) - LeetSync

--- 2519-find-the-original-array-of-prefix-xor/find-the-original-array-of-prefix-xor.cpp
+++ 2519-find-the-original-array-of-prefix-xor/find-the-original-array-of-prefix-xor.cpp
@@ -3,11 +3,9 @@ public:
     vector<int> findArray(vector<int>& pref) {
         vector<int>ans;
         ans.push_back(pref[0]);
-        int x=pref[0];
         for(int i=1;i<pref.size();i++){
-            int newans=x^pref[i];
+            int newans=pref[i-1]^pref[i];
             ans.push_back(newans);
-            x=x^newans;
         }
         return ans;
     }
