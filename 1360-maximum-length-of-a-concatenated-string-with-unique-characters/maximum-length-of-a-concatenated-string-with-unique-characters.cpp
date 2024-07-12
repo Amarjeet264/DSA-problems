746d503b075bbeb895b8a13785965aa053249c0b
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-07-12 10:22:40 +0530

    Time: 4 ms (91.88%) | Memory: 11.2 MB (83.34%) - LeetSync

--- 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,37 +1,38 @@
 class Solution {
-    unordered_map<char,int>mp;
-    bool isposs(string s){
-        unordered_map<char,int>mp1;
-        for(int i=0;i<s.size();i++){
-            if(mp1.find(s[i])!=mp1.end()||mp.find(s[i])!=mp.end()){
-                return false;
-            }
-            mp1[s[i]]++;
+    int calc(vector<int>&vec,int i,int temp){
+        if(i>=vec.size()){
+            return __builtin_popcount(temp);
         }
-        return true;
-    }
-    int maxi(int i,vector<string>&arr){
-        if(i>=arr.size()){
-            return 0;
-        }
-        int nottake=maxi(i+1,arr);
-        int take=0;
-        if(isposs(arr[i])){
-            for(int j=0;j<arr[i].size();j++){
-                mp[arr[i][j]]++;
-            }
-            take=arr[i].size()+maxi(i+1,arr);
-             for(int j=0;j<arr[i].size();j++){
-                mp[arr[i][j]]--;
-                if(mp[arr[i][j]]==0){
-                    mp.erase(arr[i][j]);
-                }
-            }
+        int nottake = calc(vec,i+1,temp);
+        int take = 0;
+        if((temp&vec[i])==0){
+            take = calc(vec,i+1,temp|vec[i]);
         }
         return max(take,nottake);
     }
 public:
     int maxLength(vector<string>& arr) {
-        return maxi(0,arr);
+        vector<string>unique;
+        for(int i=0;i<arr.size();i++){
+            string s = arr[i];
+            unordered_set<char>st;
+            for(int j=0;j<s.size();j++){
+                st.insert(s[j]);
+            }
+            if(st.size()==s.size()){
+                unique.push_back(s);
+            }
+        }
+        vector<int>vec;
+        for(int i=0;i<unique.size();i++){
+            int val = 0;
+            for(int j=0;j<unique[i].size();j++){
+                int n = unique[i][j]-'a';
+                val = val|(1<<n);
+            }
+            vec.push_back(val);
+        }
+        int temp = 0;
+        return calc(vec,0,temp);
     }
 };
