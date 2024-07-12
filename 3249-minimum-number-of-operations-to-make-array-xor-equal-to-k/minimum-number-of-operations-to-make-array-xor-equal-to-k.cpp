de8c18b01096f62ca4c8fde98f8290d6cfc10b5e
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-07-12 16:58:21 +0530

    Time: 114 ms (32.22%) | Memory: 92.1 MB (5.31%) - LeetSync

--- 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -1,49 +1,10 @@
 class Solution {
-    void con(int x,string &s){
-        while(x){
-            if(x%2==0){
-                s+='0';
-            }
-            else{
-                s+='1';
-            }
-            x=x/2;
-        }
-        reverse(s.begin(),s.end());
-    }
 public:
     int minOperations(vector<int>& nums, int k) {
-        int x=nums[0];
+        int all = nums[0];
         for(int i=1;i<nums.size();i++){
-            x=x^nums[i];
+            all ^= nums[i];
         }
-        string s="";
-        con(x,s);
-        string z="";
-        con(k,z);
-        // cout<<s<<" ";
-        int cnt=0;
-        int sz=s.size();
-        int i=0;
-        reverse(z.begin(),z.end());
-        while(s.size()>z.size()){
-            z+='0';
-        }
-        reverse(z.begin(),z.end());
-        // int i=0;
-
-        reverse(s.begin(),s.end());
-        while(s.size()<z.size()){
-            s+='0';
-        }
-        reverse(s.begin(),s.end());
-
-        while(i<s.size()){
-            if(s[i]!=z[i]){
-                cnt++;
-            }
-            i++;
-        }
-        return cnt;
+        return __builtin_popcount(all^k);
     }
 };
