de6f20e8fd3e0464cdb784e1e6409de2762f732b
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-07-14 20:12:30 +0530

    Time: 123 ms (17.32%) | Memory: 98.2 MB (18.67%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.cpp
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
     int minCost(string colors, vector<int>& need) {
-        int i=0;
         int j=1;
-        int sum=0;
-
+        int time=0;
+        int prevMax = need[0];
         while(j<colors.size()){
-            if(colors[i]==colors[j]){
-                if(need[i]<need[j]){
-                    sum+=need[i];
-                    i=j;
-                    j++;
-                }
-                else{
-                    sum+=need[j];
-                    j++;
-                }
+            if(colors[j]!=colors[j-1]){
+                prevMax = 0;
             }
-            else{
-                i=j;
-                j++;
-            }
+            time += min(prevMax,need[j]);
+            prevMax = max(prevMax,need[j]);
+            j++;
         }
-        return sum;
+        return time;
     }
 };
