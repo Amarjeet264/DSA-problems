49fa57e7a2192b6b4d00bb0c6ce18a9c10b56a55
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-08-10 10:11:57 +0530

    Time: 39 ms (10.30%) | Memory: 214.4 MB (5.15%) - LeetSync

---
+++ 999-regions-cut-by-slashes/regions-cut-by-slashes.cpp
@@ -0,0 +1,83 @@
+class dsu{
+    public:
+    vector<int>parent,rank;
+    dsu(int n){
+        parent.resize(n);
+        rank.resize(n);
+        for(int i = 0;i<n;i++){
+            parent[i]=i;
+        }
+    }
+    int findulpar(int u){
+        if(u==parent[u]){
+            return u;
+        }
+        return parent[u]=findulpar(parent[u]);
+    }
+    void unionbyr(int u,int v){
+        int ulpu = findulpar(u);
+        int ulpv = findulpar(v);
+        if(ulpv==ulpu){
+            return ;
+        }
+        if(rank[ulpv]>rank[ulpu]){
+            parent[ulpu] = ulpv;
+            rank[ulpv]+=rank[ulpu];
+        }
+        else{
+            parent[ulpv] = ulpu;
+            rank[ulpu] += rank[ulpv];
+        }
+    }
+};
+
+class Solution {
+public:
+    int regionsBySlashes(vector<string>& grid) {
+        int n = grid.size();
+        int size = (n+1)*(n+1);
+        dsu ds(size*size);
+        for(int i=1;i<=n;i++){
+            int ele = 0 * (n+1) + i;
+            ds.unionbyr(0,ele);
+        }
+        for(int i = 1;i<=n;i++){
+            int ele = i * (n+1);
+            ds.unionbyr(0,ele);
+        }
+        for(int i = 1;i<=n;i++){
+            int ele = i * (n+1) + n;
+            ds.unionbyr(0,ele);
+        }
+        for(int i = 1;i<=n;i++){
+            int ele = n * (n+1) + i;
+            ds.unionbyr(0,ele);
+        }
+        int cnt = 1;
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<n;j++){
+                if(grid[i][j]=='/'){
+                    int u = (i+1) * (n+1) + j;
+                    int v = i*(n+1) + (j+1);
+                    if(ds.findulpar(u)==ds.findulpar(v)){
+                        cnt++;
+                    }
+                    else{
+                        ds.unionbyr(u,v);
+                    }
+                }
+                else if(grid[i][j] == '\\'){
+                    int u = (i+1) * (n+1) + (j+1);
+                    int v = i*(n+1) + (j);
+                    if(ds.findulpar(u)==ds.findulpar(v)){
+                        cnt++;
+                    }
+                    else{
+                        ds.unionbyr(u,v);
+                    }
+                }
+            }
+        }
+        return cnt;
+    }
+};
