51f4105046fe7517f4e327d70badbb65c66eb1c2
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-08-20 01:48:31 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (56.27%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
     int minSteps(int n) {
-        int s=0;
-        for(int d=2;d<=n;d++){
-            while(n%d==0){
-                s+=d;
-                n/=d;
+        if (n == 1) return 0;
+        
+        int steps = 0;
+        int factor = 2;
+        
+        while (n > 1) {
+            while (n % factor == 0) {
+                steps += factor;
+                n /= factor;
             }
+            factor++;
         }
-        return s;
+        
+        return steps;
     }
 };
