7a5a45ee747631b7df16f401262ded20861c9857
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-09-13 15:52:46 +0530

    Time: 212 ms (33.10%) | Memory: 171.5 MB (26.27%) - LeetSync

---
+++ 3528-reach-end-of-array-with-max-score/reach-end-of-array-with-max-score.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long findMaximumScore(vector<int>& nums) {
+        long long ans=0;
+        long long temp=nums[0];
+        int j=0;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]>temp || i==nums.size()-1){
+                ans= ans + (long long)((i-j)*temp);
+                temp=nums[i];
+                j=i;
+            }
+        }
+        return ans;
+    }
+};
