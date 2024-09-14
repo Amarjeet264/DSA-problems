435bac5028294f36c13f5bcab558dfd85a7dc445
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-09-15 00:21:20 +0530

    Time: 64 ms (50.07%) | Memory: 11.9 MB (64.44%) - LeetSync

--- 2444-longest-ideal-subsequence/longest-ideal-subsequence.cpp
+++ 2444-longest-ideal-subsequence/longest-ideal-subsequence.cpp
@@ -17,7 +17,7 @@ public:
     int longestIdealString(string s, int k) {
         // vector<vector<int>>dp(s.size(),vector<int>(s.length()+1,-1));
         // return longest(s,k,0,s.length(),dp);
-        vector<int> dp(150,0);
+            vector<int> dp(150,0);
             int ans=0;
             for(auto &i:s){
                 for(int j=i-k;j<=i+k;j++){
@@ -25,6 +25,6 @@ public:
                 }
                 ans=max(ans,++dp[i]);
             }
-        return ans;
+            return ans;
     }
 };
