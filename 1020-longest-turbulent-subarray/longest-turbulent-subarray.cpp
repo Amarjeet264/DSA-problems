11303a23a1f601164b1859156f5325a3d1cb0887
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-09-14 21:50:23 +0530

    Time: 2887 ms (5.25%) | Memory: 69.1 MB (5.56%) - LeetSync

--- 1020-longest-turbulent-subarray/longest-turbulent-subarray.cpp
+++ 1020-longest-turbulent-subarray/longest-turbulent-subarray.cpp
@@ -1,39 +1,35 @@
 class Solution {
+    int turbu(vector<int>&arr,int idx ,int flag,vector<vector<int>>&dp){
+        if(idx>=arr.size()){
+            return 0;
+        }
+        if(dp[idx][flag]!=-1){
+            return dp[idx][flag];
+        }
+        int take = 0;
+        if(flag&&arr[idx]>arr[idx-1]){
+            take = 1+turbu(arr,idx+1,0,dp);
+        }
+        else if(!flag&&arr[idx]<arr[idx-1]){
+            take = 1+turbu(arr,idx+1,1,dp);
+        }
+        else{
+            take = 0;
+        }
+        return take;
+    }
 public:
     int maxTurbulenceSize(vector<int>& arr) {
-        int maxi = 0;
-        for(int i =0;i<arr.size();i++){
-            int len = 1;
-            bool great = 1;
-            for(int j = i+1;j<arr.size();j++){
-                if(arr[j]>arr[j-1]&&great){
-                    len++;
-                }
-                else if(arr[j]<arr[j-1]&&!great){
-                    len++;
-                }
-                else{
-                    break;
-                }
-                great = !great;
-            }
-            maxi = max(maxi,len);
-            len = 1;
-            great = 0;
-            for(int j = i+1;j<arr.size();j++){
-                if(arr[j]>arr[j-1]&&great){
-                    len++;
-                }
-                else if(arr[j]<arr[j-1]&&!great){
-                    len++;
-                }
-                else{
-                    break;
-                }
-                great = !great;
-            }
-            maxi = max(maxi,len);
+        int maxAns = 1;
+        if(arr.size()==0){
+            return 0;
         }
-        return maxi;
+        vector<vector<int>>dp(arr.size(),vector<int>(2,-1));
+        for(int i = 1;i<arr.size();i++){
+            int nextSmall = turbu(arr,i,0,dp);
+            int nextBig = turbu(arr,i,1,dp);
+            maxAns = max(maxAns,1+max(nextSmall,nextBig));
+        }
+        return maxAns;
     }
 };
