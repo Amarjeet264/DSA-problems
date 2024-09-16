039aa91bb66afdac84680df63ed90bac12dc5780
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-09-16 12:39:25 +0530

    Time: 16 ms (67.87%) | Memory: 70.3 MB (98.56%) - LeetSync

--- 2870-longest-alternating-subarray/longest-alternating-subarray.cpp
+++ 2870-longest-alternating-subarray/longest-alternating-subarray.cpp
@@ -5,8 +5,10 @@ public:
         for(int i = 0;i<nums.size();i++){
             int cnt = 1;
             bool neg = 0;
+            int idx = i;
             for(int j=i+1;j<nums.size();j++){
                 if(!neg&&nums[j]-nums[j-1]==1){
+                    i = j;
                     cnt++;
                 }
                 else if(neg&&nums[j]-nums[j-1]==-1){
@@ -16,6 +18,9 @@ public:
                     break;
                 }
                 neg = !neg;
+            }
+            if(i!=idx){
+                i--;
             }
             maxi = max(maxi ,cnt);
         }
