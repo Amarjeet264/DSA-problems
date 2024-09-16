c7290fd11ff0972f1a685c6b83d6241b3478c602
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-09-16 12:30:01 +0530

    Time: 29 ms (10.47%) | Memory: 70.5 MB (67.87%) - LeetSync

---
+++ 2870-longest-alternating-subarray/longest-alternating-subarray.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int alternatingSubarray(vector<int>& nums) {
+        int maxi = 0;
+        for(int i = 0;i<nums.size();i++){
+            int cnt = 1;
+            bool neg = 0;
+            for(int j=i+1;j<nums.size();j++){
+                if(!neg&&nums[j]-nums[j-1]==1){
+                    cnt++;
+                }
+                else if(neg&&nums[j]-nums[j-1]==-1){
+                    cnt++;
+                }
+                else{
+                    break;
+                }
+                neg = !neg;
+            }
+            maxi = max(maxi ,cnt);
+        }
+
+        return maxi==1?-1:maxi;
+    }
+};
