e3ae175d13bb19f5ffe0a5dbb28fce149a5cec60
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-10-08 01:34:07 +0530

    Time: 180 ms (69.89%) | Memory: 244.5 MB (11.08%) - LeetSync

--- 472-concatenated-words/concatenated-words.cpp
+++ 472-concatenated-words/concatenated-words.cpp
@@ -1,36 +1,25 @@
 struct Node {
     bool end = 0;
-    Node* Links[26] = {nullptr};  // Initialize with nullptrs
+    Node* Links[26] = {nullptr};
 
-    bool containsKey(char ch) {
-        return Links[ch - 'a'] != nullptr;
-    }
+    bool containsKey(char ch) { return Links[ch - 'a'] != nullptr; }
 
-    void put(char ch, Node* node) {
-        Links[ch - 'a'] = node;
-    }
+    void put(char ch, Node* node) { Links[ch - 'a'] = node; }
 
-    Node* next(char ch) {
-        return Links[ch - 'a'];
-    }
+    Node* next(char ch) { return Links[ch - 'a']; }
 
-    bool isEnd() {
-        return end;
-    }
+    bool isEnd() { return end; }
 
-    void setEnd() {
-        end = 1;
-    }
+    void setEnd() { end = 1; }
 };
 
 class TrieNode {
     Node* root;
-public:
-    TrieNode() {
-        root = new Node();
-    }
 
-    void insert(std::string word) {
+public:
+    TrieNode() { root = new Node(); }
+
+    void insert(string word) {
         Node* curr = root;
         for (int i = 0; i < word.size(); i++) {
             if (!curr->containsKey(word[i])) {
@@ -40,46 +29,40 @@ public:
         }
         curr->setEnd();
     }
-    
-    bool count(std::string word, int i, int parts,vector<vector<int>>&dp) {
-        if (i == word.size()) {
-            return parts >= 2;  // Word must be formed by at least two parts
-        }
-        if(dp[i][parts]!=-1){
-            return dp[i][parts];
-        }
+
+    int count(string word, int i, vector<int>& dp) {
+        if (i == word.size())
+            return 0;
+        if (dp[i] != -1)
+            return dp[i];
+
+        int ans = -1e9;
         Node* curr = root;
         for (int j = i; j < word.size(); j++) {
             if (!curr->containsKey(word[j])) {
-                return dp[i][parts] = false;
+                break;
             }
             curr = curr->next(word[j]);
             if (curr->isEnd()) {
-                // Try to split here and count further parts
-                if (count(word, j + 1, parts + 1,dp)) {
-                    return dp[i][parts] = true;
-                }
+                ans = max(ans, 1 + count(word, j + 1, dp));
             }
         }
-        return dp[i][parts] = false;
+        return dp[i] = ans;
     }
 };
 
 class Solution {
 public:
-    std::vector<std::string> findAllConcatenatedWordsInADict(std::vector<std::string>& words) {
+    vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
         TrieNode trie;
-        // Insert all words into the Trie
         for (int i = 0; i < words.size(); i++) {
             trie.insert(words[i]);
         }
 
-        std::vector<std::string> ans;
-        // Check if each word can be formed by concatenating two or more words
+        vector<string> ans;
         for (int i = 0; i < words.size(); i++) {
-            vector<vector<int>>dp(words[i].size(),vector<int>(words[i].size(),-1));
-            if (trie.count(words[i], 0, 0,dp)) {
-
+            vector<int> dp(words[i].size(), -1);
+            if (trie.count(words[i], 0, dp) >= 2) {
                 ans.push_back(words[i]);
             }
         }
