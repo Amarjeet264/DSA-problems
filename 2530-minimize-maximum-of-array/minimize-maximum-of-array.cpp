4f6f79789f0d4eccbfd7ae2d2d13a182c27cc22c
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-10-11 00:45:17 +0530

    Time: 167 ms (5.15%) | Memory: 311.5 MB (5.15%) - LeetSync

--- 2530-minimize-maximum-of-array/minimize-maximum-of-array.cpp
+++ 2530-minimize-maximum-of-array/minimize-maximum-of-array.cpp
@@ -5,20 +5,17 @@ class Solution {
             nums.push_back((long long)arr[i]);
         }
         long long buff = 0;
-        for(int i=0;i<nums.size()-1;i++){
-            // if(nums[i]>mid&&nums[i]-mid>buff){
-            //     return 0;
-            // }
-            // nums[i] = nums[i] - buff;
-            // if(nums[i]<0){
-            //     nums[i] = 0;
-            // }
-            // buff = mid - nums[i];
-            if(nums[i]>mid)return 0;
-            buff = mid-nums[i];
-            nums[i+1] = nums[i+1]-buff;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>mid&&nums[i]-mid>buff){
+                return 0;
+            }
+            nums[i] = nums[i] - buff;
+            buff = mid - nums[i];
+            // if(nums[i]>mid)return 0;
+            // buff = mid-nums[i];
+            // nums[i+1] = nums[i+1]-buff;
         }
-        return nums[nums.size()-1]<=mid;
+        return 1;
     }
 public:
     int minimizeArrayValue(vector<int>& nums) {
