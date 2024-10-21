3986107e7e619d131f8c74ddd2497bfdca872eae
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-10-21 17:37:27 +0530

    Time: 128 ms (97.00%) | Memory: 108.5 MB (19.93%) - LeetSync

--- 931-maximum-frequency-stack/maximum-frequency-stack.cpp
+++ 931-maximum-frequency-stack/maximum-frequency-stack.cpp
@@ -1,39 +1,34 @@
 class FreqStack {
     int id = 0;
-    unordered_map<int, int> ele;  // Frequency of each element
-    set<pair<int, pair<int, int>>> st;  // Store (-frequency, -id, element) to handle sorting
-    map<int, vector<int>> idx;  // Store insertion order (id) for each element
-
+    unordered_map<int,int>ele;
+    set<pair<int,pair<int,int>>>st;
+    map<int,vector<int>>idx;
 public:
-    FreqStack() {}
+    FreqStack() {
+        
+    }
     
     void push(int val) {
-        ele[val] += 1;  // Increase frequency of element
-        st.insert({-ele[val], {-id, val}});  // Insert into set with (-frequency, -id, val)
-        idx[val].push_back(id);  // Store the id for the element
-        id++;  // Increment id for next element
+        ele[val] += 1;
+        st.insert({-ele[val],{-id,val}});
+        idx[val].push_back(id);
+        id++;
     }
     
     int pop() {
-        // Get the element with highest frequency and latest insertion order
-        auto maxP = *st.begin();
-        st.erase(maxP);  // Remove the entry from the set
-        int val = maxP.second.second;  // The value of the element to pop
-
-        // Remove the latest index for the popped element
+        pair<int,pair<int,int>>maxP = *st.begin();
+        st.erase(maxP);
+        int val = maxP.second.second;
+        ele[val] -=1;
         idx[val].pop_back();
-        
-        // Decrease the frequency of the element
-        ele[val]--;
-        
-        // If the frequency drops to 0, remove it completely
-        if (ele[val] == 0) {
-            ele.erase(val);
+        if(ele[val]==0){
             idx.erase(val);
-        } else {
-            // Insert updated frequency and latest id into the set
-            int newId = idx[val].back();  // Get the new latest id after popping
-            st.insert({-ele[val], {-newId, val}});
+            ele.erase(val);
+        }
+        else{
+            int newIdx = idx[val].back();
+
+            st.insert({-ele[val],{-newIdx,val}});
         }
         return val;
     }
