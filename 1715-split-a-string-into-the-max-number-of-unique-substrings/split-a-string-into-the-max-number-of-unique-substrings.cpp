6ecfcad3da3bab5cbb8dad6c27b3e14d69051047
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-10-21 14:47:20 +0530

    Time: 270 ms (41.23%) | Memory: 60.6 MB (47.08%) - LeetSync

--- 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.cpp
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -1,24 +1,25 @@
 class Solution {
     int maxi = 1;
-    void call(int i,string s ,unordered_set<string>st,int cnt){
+    int call(int i,string s ,unordered_set<string>&st){
         if(i>=s.size()){
-            maxi = max(maxi,cnt);
-            return;
+            return 0;
         }
         string str = "";
+        int maxi = 0;
         for(int j = i;j<s.size();j++){
             str+=s[j];
             if(st.find(str)==st.end()){
                 st.insert(str);
-                call(j+1,s,st,cnt+1);
+                maxi = max(maxi,1+call(j+1,s,st));
                 st.erase(str);
             }
         }
+        return maxi;
     }
 public:
     int maxUniqueSplit(string s) {
         unordered_set<string>st;
-        call(0,s,st,0);
-        return maxi;
+        return call(0,s,st);
+        // return maxi;
     }
 };
