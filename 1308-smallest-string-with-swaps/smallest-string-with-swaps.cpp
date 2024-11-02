1a2d32e66aea678e7de23c802a9d845a480616f9
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-11-02 21:20:45 +0530

    Time: 331 ms (21.32%) | Memory: 83.8 MB (9.78%) - LeetSync

--- 1308-smallest-string-with-swaps/smallest-string-with-swaps.cpp
+++ 1308-smallest-string-with-swaps/smallest-string-with-swaps.cpp
@@ -1,43 +1,44 @@
 class Solution {
-    void dfs(vector<int>&vis,int node,vector<int>adj[],vector<int>&x){
-        x.push_back(node);
+    void dfs(vector<int> &dup,int node,vector<int>adj[],vector<int>&vis){
         vis[node]=1;
+        dup.push_back(node);
         for(auto it:adj[node]){
             if(!vis[it]){
-                dfs(vis,it,adj,x);
+                dfs(dup,it,adj,vis);
             }
         }
     }
 public:
     string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) {
-        vector<int>adj[s.length()];
+        int n = s.size();
+        vector<int>adj[n];
         for(auto it:pairs){
             adj[it[0]].push_back(it[1]);
             adj[it[1]].push_back(it[0]);
         }
-        vector<int>vis(s.length(),0);
+        vector<int>vis(n);
         vector<vector<int>>vec;
-        for(int i=0;i<s.length();i++){
-            vector<int>x;
-            if(!vis[i])dfs(vis,i,adj,x);
-            vec.push_back(x);
+        for(int i =0;i<n;i++){
+            if(!vis[i]){
+                vector<int>x;
+                dfs(x,i,adj,vis);
+                vec.push_back(x);
+            }
         }
         unordered_map<int,char>mp;
-        for(int i=0;i<vec.size();i++){
-            sort(vec[i].begin(),vec[i].end());
-            string x="";
-            for(int j=0;j<vec[i].size();j++){
+        for(int i =0;i<vec.size();i++){
+            string x = "";
+            for(int j =0;j<vec[i].size();j++){
                 x+=s[vec[i][j]];
-                // cout<<vec[i][j]<<" ";
             }
-            // cout<<endl;
             sort(x.begin(),x.end());
-            for(int j=0;j<x.size();j++){
-                mp[vec[i][j]]=x[j];
+            sort(vec[i].begin(),vec[i].end());
+            for(int j=0;j<vec[i].size();j++){
+                mp[vec[i][j]] = x[j];
             }
         }
-        string ans="";
-        for(int i=0;i<s.length();i++){
+        string ans = "";
+        for(int i =0;i<n;i++){
             ans+=mp[i];
         }
         return ans;
