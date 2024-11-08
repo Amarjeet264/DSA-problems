e60975383c599a6b38e61d1384ec61a593ed7dac
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-11-09 00:22:36 +0530

    Time: 3 ms (70.84%) | Memory: 11.7 MB (56.06%) - LeetSync

--- 133-clone-graph/clone-graph.cpp
+++ 133-clone-graph/clone-graph.cpp
@@ -20,41 +20,30 @@ public:
 */
 
 class Solution {
-    void clone(Node* root1,unordered_map<int,Node*>&mp,Node* curr){
-        // Node* root1=new Node(curr->val);
-        for(int i=0;i<curr->neighbors.size();i++){
-            Node* curr1=curr->neighbors[i];
-            if(mp[curr1->val]==nullptr){
-                Node* nayacurr1=new Node(curr1->val);
-                mp[curr1->val]=nayacurr1;
-                root1->neighbors.push_back(nayacurr1);
-                clone(nayacurr1,mp,curr1);
-            }
-            else{
-                 root1->neighbors.push_back(mp[curr1->val]);
-            } 
-        }
-    }
 public:
     Node* cloneGraph(Node* node) {
-        if(!node){
-            return nullptr;
-        }
-        Node* root=new Node(node->val);
-        unordered_map<int,Node*>mp;
-        mp[root->val]=root;
-        for(int i=0;i<node->neighbors.size();i++){
-            Node* curr=node->neighbors[i];
-            if(mp[curr->val]==0){
-                Node* nayacurr=new Node(curr->val);
-                mp[curr->val]=nayacurr;
-                root->neighbors.push_back(nayacurr);
-                clone(nayacurr,mp,curr);
+        if(!node)return nullptr;
+        Node * naya = new Node();
+        naya->val = node->val;
+        unordered_map<Node*,Node*>mp;
+        mp[node] = naya;
+        queue<Node*>q;
+        q.push(node);
+        while(!q.empty()){
+            Node* root = q.front();
+            q.pop();
+            vector<Node*>neigh = root->neighbors;
+            int n = neigh.size();
+            for(int i=0;i<n;i++){
+                if(mp[neigh[i]] == nullptr){
+                    naya = new Node();
+                    naya->val = neigh[i]->val;
+                    mp[neigh[i]] = naya;
+                    q.push(neigh[i]);
+                }
+                mp[root]->neighbors.push_back(mp[neigh[i]]);
             }
-            else{
-                root->neighbors.push_back(mp[curr->val]);
-            }
         }
-        return root;
+        return mp[node];
     }
 };
