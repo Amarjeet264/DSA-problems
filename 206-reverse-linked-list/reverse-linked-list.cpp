6dca4fb42ed4d8ea2d0fdfce92b48edbf4659563
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-11-11 01:36:49 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (8.53%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -23,8 +23,29 @@ class Solution {
     }
 public:
     ListNode* reverseList(ListNode* head) {
-        if(!head||!head->next)return head;
-        rev(head);
-        return ans;
+        if (head == NULL || head->next == NULL) {
+             return head;
+        }
+            
+            // Recursive step:
+            // Reverse the linked list starting 
+            // from the second node (head->next).
+            ListNode* newHead = reverseList(head->next);
+            
+            // Save a reference to the node following
+            // the current 'head' node.
+            ListNode* front = head->next;
+            
+            // Make the 'front' node point to the current
+            // 'head' node in the reversed order.
+            front->next = head;
+            
+            // Break the link from the current 'head' node
+            // to the 'front' node to avoid cycles.
+            head->next = NULL;
+            
+            // Return the 'newHead,' which is the new
+            // head of the reversed linked list.
+            return newHead;
     }
 };
