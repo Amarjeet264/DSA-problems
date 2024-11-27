4f0f75fd6426f8bf634fd27342420022cf0c4bd4
Amarjeet264 <100895607+Amarjeet264@users.noreply.github.com>
2024-11-27 17:16:30 +0530

    Time: 199 ms (46.67%) | Memory: 116.4 MB (59.51%) - LeetSync

--- 3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.cpp
+++ 3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.cpp
@@ -1,49 +1,37 @@
 class Solution {
-    int bfs(int src, int dst, vector<int> adj[], int n) {
-        vector<int> dis(n, INT_MAX); // Initialize distances to infinity
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        
-        // Start BFS
-        dis[src] = 0;
-        pq.push({0, src});
-        
-        while (!pq.empty()) {
+    int bfs(int src,int dst,vector<int>adj[],int n){
+        priority_queue<pair<int,int>>pq;
+        pq.push({0,0});
+        vector<int>dis(n,INT_MAX);
+        dis[0] = 0;
+        while(!pq.empty()){
             int node = pq.top().second;
-            int d = pq.top().first;
+            int d = -pq.top().first;
             pq.pop();
-            
-            // If we reach the destination, return the distance
-            if (node == dst) return d;
-            
-            // Relax neighbors
-            for (auto it : adj[node]) {
-                if (dis[node] + 1 < dis[it]) {
+            if(node==dst){
+                return d;
+            }
+            for(auto it:adj[node]){
+                if(dis[node]+1<dis[it]){
                     dis[it] = dis[node] + 1;
-                    pq.push({dis[it], it});
+                    pq.push({-dis[it],it});
                 }
             }
         }
-        return -1; // If destination is unreachable
+        return -1;
     }
-
 public:
     vector<int> shortestDistanceAfterQueries(int n, vector<vector<int>>& queries) {
-        vector<int> adj[n];
-        
-        // Build the initial graph
-        for (int i = 0; i < n - 1; i++) {
-            adj[i].push_back(i + 1);
+        vector<int>adj[n];
+        for(int i=0;i<n-1;i++){
+            adj[i].push_back(i+1);
         }
-        
-        vector<int> ans;
-        
-        // Process each query
-        for (const auto& query : queries) {
-            adj[query[0]].push_back(query[1]); // Add the new edge
-            int x = bfs(0, n - 1, adj, n);     // Calculate shortest distance
-            ans.push_back(x);                  // Store the result
+        vector<int>ans;
+        for(int i = 0;i<queries.size();i++){
+            adj[queries[i][0]].push_back(queries[i][1]);
+            int x = bfs(0,n-1,adj,n);
+            ans.push_back(x);
         }
-        
         return ans;
     }
 };
